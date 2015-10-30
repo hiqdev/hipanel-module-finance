685eb2cb31a421ddf57ab238c893b19a6a7b8895
Andrii Vasyliev <sol@hiqdev.com>
2015-10-30 10:39:16 +0000

    + Merchant module

---
+++ src/models/Merchant.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Finance Plugin for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-finance
+ * @package   hipanel-module-finance
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2014-2015, HiQDev (https://hiqdev.com/)
+ */
+
+namespace hipanel\modules\finance\models;
+
+class Merchant extends \hipanel\base\Model
+{
+    use \hipanel\base\ModelTrait;
+    use \hiqdev\yii2\merchant\models\MerchantTrait;
+}
