6c6aebdfa322831b0c78b74c828f1cca51a04f01
Andrii Vasyliev <sol@hiqdev.com>
2015-11-30 11:22:55 +0000

    + local payment methods block

---
+++ src/views/payment-methods.php
@@ -0,0 +1,20 @@
+<?php
+
+\hiqdev\paymenticons\yii2\PaymentIconsAsset::register($this);
+
+?>
+<p class="lead"><?= Yii::t('app', 'Payment Methods') ?>:</p>
+
+<div class="payment-icon-xs paypal">&nbsp;&nbsp;</div>
+<div class="payment-icon-xs visa">&nbsp;&nbsp;</div>
+
+<img src="https://almsaeedstudio.com/themes/AdminLTE/dist/img/credit/visa.png" alt="Visa">
+<img src="https://almsaeedstudio.com/themes/AdminLTE/dist/img/credit/mastercard.png" alt="Mastercard">
+<img src="https://almsaeedstudio.com/themes/AdminLTE/dist/img/credit/american-express.png"
+     alt="American Express">
+<img src="https://almsaeedstudio.com/themes/AdminLTE/dist/img/credit/paypal2.png" alt="Paypal">
+
+<p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+    Hello, etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem plugg
+    dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
+</p>
