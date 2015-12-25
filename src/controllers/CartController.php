d4c6d867aa8462558caadf71e25fbab91934701f
Andrii Vasyliev <sol@hiqdev.com>
2015-12-25 09:58:50 +0000

    used finishUrl instead of returnUrl

--- src/controllers/CartController.php
+++ src/controllers/CartController.php
@@ -19,8 +19,8 @@ class CartController extends \yii\web\Controller
     public function renderDeposit($sum)
     {
         return $this->module->getMerchant()->renderDeposit([
-            'sum' => $sum,
-            'returnUrl' => '/finance/cart/finish',
+            'sum'       => $sum,
+            'finishUrl' => '/finance/cart/finish',
         ]);
     }
 
