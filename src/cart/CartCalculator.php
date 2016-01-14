a62c53e92bfcf8139bc5640e7b19185ea876bbac
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-01-14 19:25:10 +0200

    CartCalculator renamed to CartCalculation

---
+++ src/cart/CartCalculator.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace hipanel\modules\finance\cart;
+
+use hipanel\modules\finance\models\Calculation;
+use hiqdev\hiart\ErrorResponseException;
+use hiqdev\yii2\cart\ShoppingCart;
+use Yii;
+use yii\base\Object;
+use yii\web\UnprocessableEntityHttpException;
+use yz\shoppingcart\CartActionEvent;
+
+class CartCalculator extends Object
+{
+    /**
+     * @var ShoppingCart
+     */
+    public $cart;
+
+    /**
+     * @var CartActionEvent
+     */
+    public $event;
+
+    /**
+     * @var AbstractCartPosition
+     */
+    public $position;
+
+    /**
+     * Creates the instance of the object and runs the calculation
+     *
+     * @param CartActionEvent $event The event
+     * @void
+     */
+    public static function execute($event)
+    {
+        $calculator = new static(['event' => $event]);
+        $calculator->run();
+    }
+
+    /** @inheritdoc */
+    public function init()
+    {
+        $this->cart = $this->event->sender;
+        $this->position = $this->event->position;
+    }
+
+    /**
+     * Runs the calculation.
+     * Normally, the method should call [[calculateValue]]
+     * @void
+     */
+    public function run()
+    {
+        $this->calculateValue($this->getCalculationModels());
+    }
+
+    /**
+     * The method sends the request to the billing and update positions
+     *
+     * @param Calculation[] $models
+     * @see updatePositions
+     * @throws UnprocessableEntityHttpException
+     */
+    protected function calculateValue($models)
+    {
+        $data = [];
+
+        foreach ($models as $model) {
+            $data[$model->getPrimaryKey()] = $model->getAttributes();
+        }
+
+        $response = $this->sendRequest($data);
+        $this->updatePositions($response);
+    }
+
+    /**
+     * Sends request to the billing API
+     *
+     * @param array $data cart items structured properly for the billing API
+     * @return array
+     * @throws UnprocessableEntityHttpException
+     */
+    private function sendRequest($data)
+    {
+        if (empty($data)) {
+            return [];
+        }
+
+        try {
+            $result = Calculation::perform('CalcValue', $data, true);
+        } catch (ErrorResponseException $e) {
+            $result = $e->errorInfo['response'];
+        } catch (\Exception $e) {
+            throw new UnprocessableEntityHttpException('Failed to calculate cart value', 0, $e);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Updates positions with the value from $data
+     *
+     * @param array $data
+     */
+    private function updatePositions($data)
+    {
+        foreach ($this->cart->positions as $id => $position) {
+            if (isset($data[$id])) {
+                $value = reset($data[$id]['value']); // data is wrapped with currency. todo: dynamic currencies
+                $position->setPrice($value['price']);
+                $position->setValue($value['value']);
+            } else {
+                Yii::warning('Cart position was removed from the cart because of failed value calculation', 'hipanel.cart');
+                $this->cart->removeById($position->id);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Collects the calculation models form the cart positions
+     *
+     * @return Calculation[]
+     */
+    protected function getCalculationModels()
+    {
+        $models = [];
+
+        foreach ($this->cart->positions as $position) {
+            $models[$position->id] = $position->getCalculationModel();
+        }
+
+        return $models;
+    }
+}
