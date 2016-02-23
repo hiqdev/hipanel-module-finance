0f40b6751410b47474298f09cad220b786373095
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-02-23 18:29:01 +0200

    Added RUse model

---
+++ src/models/RUse.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * Finance Plugin for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-finance
+ * @package   hipanel-module-finance
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\finance\models;
+
+use Yii;
+
+/**
+ * Class RUse. Used to represent Resources Usage
+ * @package hipanel\modules\finance\models
+ */
+class RUse extends \hipanel\base\Model
+{
+    use \hipanel\base\ModelTrait;
+
+    public function formName()
+    {
+        return 'Use';
+    }
+
+    public static function index()
+    {
+       return 'Uses';
+    }
+
+    public static function type()
+    {
+       return 'Use';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'type_id', 'last', 'total'], 'integer'],
+            [['date', 'type', 'aggregation'], 'safe'],
+        ];
+    }
+
+    public function getDisplayDate()
+    {
+        if ($this->aggregation === 'month') {
+            return Yii::$app->formatter->asDate(strtotime($this->date), 'LLL y');
+        } elseif ($this->aggregation === 'week') {
+            return Yii::$app->formatter->asDate(strtotime($this->date), 'dd LLL y');
+        } elseif ($this->aggregation === 'day') {
+            return Yii::$app->formatter->asDate(strtotime($this->date), 'dd LLL y');
+        }
+
+        return Yii::$app->formatter->asDate(strtotime($this->date));
+    }
+}
