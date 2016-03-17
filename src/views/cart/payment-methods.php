04f8df29d20d8862007b90b64e18e5fe93161a91
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-03-17 17:35:43 +0200

    Cart payment methods do not throw warning when no methods available

--- src/views/cart/payment-methods.php
+++ src/views/cart/payment-methods.php
@@ -2,10 +2,13 @@
 
 \hiqdev\paymenticons\yii2\PaymentIconsAsset::register($this);
 
+$methods = [];
+
 $provides = [
-    'paypal'     => ['visa', 'maestro2'],
+    'paypal' => ['visa', 'maestro2'],
     'interkassa' => ['visa', 'maestro2'],
 ];
+
 foreach ($merchants as $merchant) {
     $name = strtolower($merchant->gateway);
     $methods[] = $name;
@@ -18,14 +21,12 @@ foreach ($merchants as $merchant) {
 $methods = array_unique($methods);
 
 ?>
-<p class="lead"><?= Yii::t('cart', 'Payment methods') ?>:</p>
+    <p class="lead"><?= Yii::t('cart', 'Payment methods') ?>:</p>
 
-<?php foreach ($methods as $name) : ?>
-    <i class="pi pi-<?= strtolower($name) ?>"></i>
-<?php endforeach ?>
-
-<?php /*
-<p class="text-muted well well-sm no-shadow" style="margin-top: 10px">
-    <?= Yii::t('hipanel/finance', 'Direct payments') ?>
-</p>
-*/ ?>
+<?php if (empty($methods)) :
+    echo Yii::t('cart', 'No available payment methods');
+else :
+    foreach ($methods as $name) : ?>
+        <i class="pi pi-<?= strtolower($name) ?>"></i>
+    <?php endforeach;
+endif; ?>
