c289c752b2272d818d4e8fac2dfc1f7c6dafaedf
Andrii Vasyliev <sol@hiqdev.com>
2016-05-18 10:56:43 +0000

    used composer-config-plugin

---
+++ src/config/hisite.php
@@ -0,0 +1,61 @@
+<?php
+
+$params = require COMPOSER_CONFIG_PLUGIN_DIR . '/params.php';
+
+return [
+    'aliases' => [
+        '@bill'   => '/finance/bill',
+        '@purse'  => '/finance/purse',
+        '@tariff' => '/finance/tariff',
+        '@pay'    => '/merchant/pay',
+        '@cart'   => '/cart/cart',
+    ],
+    'menus' => [
+        'hipanel\modules\finance\SidebarMenu',
+    ],
+    'modules' => [
+        'finance' => [
+            'class' => 'hipanel\modules\finance\Module',
+        ],
+        'cart' => [
+            'class'          => 'hiqdev\yii2\cart\Module',
+            'termsPage'      => $params['orgUrl'] . 'rules',
+            'orderPage'      => '/finance/cart/select',
+            /*'orderButton'    => function ($module) {
+                return Yii::$app->getView()->render('@hipanel/modules/finance/views/cart/order-button', [
+                    'module' => $module,
+                ]);
+            },*/
+            'paymentMethods' => function () {
+                return Yii::$app->getView()->render('@hipanel/modules/finance/views/cart/payment-methods', [
+                    'merchants' => Yii::$app->getModule('merchant')->getCollection([])->getItems(),
+                ]);
+            },
+            'shoppingCartOptions' => [
+                'on cartChange' => ['hipanel\modules\finance\cart\CartCalculator', 'execute'],
+            ],
+        ],
+        'merchant' => [
+            'class'           => 'hiqdev\yii2\merchant\Module',
+            'returnPage'      => '/finance/pay/return',
+            'notifyPage'      => '/finance/pay/notify',
+            'finishPage'      => '/finance/bill',
+            'depositClass'    => 'hipanel\modules\finance\merchant\Deposit',
+            'collectionClass' => 'hipanel\modules\finance\merchant\Collection',
+        ],
+    ],
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'hipanel/finance' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@hipanel/modules/finance/messages',
+                    'fileMap' => [
+                        'hipanel/finance' => 'finance.php',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
+}
