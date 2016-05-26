13f02fb4f313a04b3f0e5bcd4e56c8aa795d37b6
tofid <andreyklochok@gmail.com>
2016-05-26 18:16:23 +0300

    Rdesign cart/select page

--- src/views/cart/select.php
+++ src/views/cart/select.php
@@ -6,39 +6,50 @@
  * @var \hiqdev\yii2\cart\ShoppingCart $cart
  */
 use yii\helpers\Html;
-
+$this->title = Yii::t('hipanel/finance', 'Select');
 ?>
 
-<h1><?= Yii::t('hipanel/finance', 'You balance: {balance} {formattedCredit}', [
-        'balance' => $cart->formatCurrency($client->balance),
-        'formattedCredit' => $client->credit > 0 ? Yii::t('hipanel/finance', '(+{amount} of credit)', ['amount' => $cart->formatCurrency($client->credit)]) : ''
-    ]) ?>:
-</h1>
+<div class="row">
+    <div class="col-md-offset-4 col-md-4">
 
-<?php if ($rest >= $cart->total) : ?>
-    <h3><?= Yii::t('hipanel/finance', 'It\'s enough to pay your cart') ?></h3>
-    <br>
-    <p>
-        <?= Html::a(Yii::t('hipanel/finance', 'Use credit funds without depositing'), '@finance/cart/finish', [
-            'class' => 'btn btn-lg btn-primary',
-            'style' => 'width:37em'
-        ]) ?>
-    </p>
-<?php else : ?>
-    <h3><?= Yii::t('hipanel/finance', 'You can pay your cart partially') ?>:</h3>
-    <br>
-    <p>
-        <?php $text = Yii::t('hipanel/finance', 'Use credit funds and pay the difference {amount}', [
-            'amount' => $cart->formatCurrency($cart->total - $rest)
-        ]);
-        echo Html::a($text, '@finance/cart/partial', ['class' => 'btn btn-lg btn-primary', 'style' => 'width:37em']) ?>
-    </p>
-<?php endif ?>
-<?php if ($cart->total > 0) : ?>
-    <p>
-        <?php $text = Yii::t('hipanel/finance', 'Do not use credit funds, pay the whole cart: {amount}', [
-            'amount' => $cart->formatCurrency($cart->total)
-        ]);
-        echo Html::a($text, '@finance/cart/full', ['class' => 'btn btn-lg btn-primary', 'style' => 'width:37em']) ?>
-    </p>
-<?php endif ?>
+        <div class="box box-solid">
+            <div class="box-header with-border text-center">
+                <h3 class="box-title">
+                    <?= Yii::t('hipanel/finance', 'You balance: {balance} {formattedCredit}', [
+                        'balance' => $cart->formatCurrency($client->balance),
+                        'formattedCredit' => $client->credit > 0 ? Yii::t('hipanel/finance', '(+{amount} of credit)', ['amount' => $cart->formatCurrency($client->credit)]) : ''
+                    ]) ?>:
+                </h3>
+            </div>
+            <div class="box-body">
+                <?php if ($rest >= $cart->total) : ?>
+                    <h3><?= Yii::t('hipanel/finance', 'It\'s enough to pay your cart') ?></h3>
+                    <br>
+                    <p>
+                        <?= Html::a(Yii::t('hipanel/finance', 'Use credit funds without depositing'), '@finance/cart/finish', [
+                            'class' => 'btn btn-lg btn-primary btn-block'
+                        ]) ?>
+                    </p>
+                <?php else : ?>
+                    <h3><?= Yii::t('hipanel/finance', 'You can pay your cart partially') ?>:</h3>
+                    <br>
+                    <p>
+                        <?php $text = Yii::t('hipanel/finance', 'Use credit funds and pay the difference {amount}', [
+                            'amount' => $cart->formatCurrency($cart->total - $rest)
+                        ]);
+                        echo Html::a($text, '@finance/cart/partial', ['class' => 'btn btn-lg btn-primary  btn-block']) ?>
+                    </p>
+                <?php endif ?>
+                <?php if ($cart->total > 0) : ?>
+                    <p>
+                        <?php $text = Yii::t('hipanel/finance', 'Do not use credit funds, pay the whole cart: {amount}', [
+                            'amount' => $cart->formatCurrency($cart->total)
+                        ]);
+                        echo Html::a($text, '@finance/cart/full', ['class' => 'btn btn-lg btn-primary btn-block']) ?>
+                    </p>
+                <?php endif ?>
+            </div>
+        </div>
+
+    </div>
+</div>
