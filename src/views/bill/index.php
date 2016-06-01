670e60ce454ef9280a3c42514ef6884b31576d1d
tofid <andreyklochok@gmail.com>
2016-06-01 14:14:34 +0300

    Change index page layout

--- src/views/bill/index.php
+++ src/views/bill/index.php
@@ -2,52 +2,63 @@
 
 use hipanel\modules\finance\grid\BillGridView;
 use hipanel\widgets\ActionBox;
+use hipanel\widgets\IndexLayoutSwitcher;
+use hipanel\widgets\IndexPage;
+use hipanel\widgets\Pjax;
 use yii\helpers\Html;
 
 $this->title                   = Yii::t('app', 'Bills');
 $this->params['breadcrumbs'][] = $this->title;
-$this->subtitle = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
+$this->subtitle = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list'); ?>
 
-?>
+<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
+    <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
 
-<?php $box = ActionBox::begin(['model' => $model, 'dataProvider' => $dataProvider, 'bulk' => true]) ?>
-    <?php $box->beginActions() ?>
-        <?php
-            if (Yii::$app->user->can('manage')) {
-                echo $box->renderCreateButton(Yii::t('app', 'Add payment')) . '&nbsp;';
-            }
-            echo Html::a(Yii::t('hipanel/finance', 'Recharge account'), ['@pay/deposit'], ['class' => 'btn btn-success']);
-        ?>
-        <?= $box->renderSearchButton() ?>
-        <?= $box->renderSorter([
-            'attributes' => [
-                'seller',
-                'client',
-                'sum',
-                'balance',
-                'type',
-                'descr',
-            ],
-        ]) ?>
-        <?= $box->renderPerPage(); ?>
-    <?php $box->endActions() ?>
-    <?= $box->renderBulkActions([
-        'items' => [
-//            TODO: implement bills edit
-//            Yii::$app->user->can('manage')       ? $box->renderBulkButton(Yii::t('app', 'Edit'), 'edit') : null,
-            Yii::$app->user->can('delete-bills') ? $box->renderDeleteButton() : null,
-        ],
-    ]) ?>
-    <?= $box->renderSearchForm(compact('type')) ?>
-<?php $box->end() ?>
+        <?= $page->setSearchFormData(compact('type')) ?>
 
-<?php $box->beginBulkForm() ?>
-    <?= BillGridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel'  => $model,
-        'columns'      => [
-            'checkbox', 'client_id', 'time', 'sum', 'balance',
-            'type_label', 'description',
-        ],
-    ]) ?>
-<?php $box->endBulkForm() ?>
+        <?php $page->beginContent('main-actions') ?>
+            <?php if (Yii::$app->user->can('manage')) : ?>
+                <?= Html::a(Yii::t('hipanel/finance', 'Add payment'), 'create', ['class' => 'btn btn-sm btn-success']) ?>
+            <?php endif; ?>
+            <?= Html::a(Yii::t('hipanel/finance', 'Recharge account'), ['@pay/deposit'], ['class' => 'btn btn-sm btn-success']) ?>
+        <?php $page->endContent() ?>
+
+        <?php $page->beginContent('show-actions') ?>
+            <?= IndexLayoutSwitcher::widget() ?>
+            <?= $page->renderSorter([
+                'attributes' => [
+                    'seller',
+                    'client',
+                    'sum',
+                    'balance',
+                    'type',
+                    'descr',
+                ],
+            ]) ?>
+            <?= $page->renderPerPage() ?>
+            <?= $page->renderRepresentation() ?>
+        <?php $page->endContent() ?>
+
+        <?php $page->beginContent('bulk-actions') ?>
+            <?php
+            // TODO: implement bills edit
+//            if (Yii::$app->user->can('manage')) print $page->renderBulkButton(Yii::t('app', 'Edit'), 'edit');
+            ?>
+            <?php if (Yii::$app->user->can('delete-bills')) print $page->renderBulkButton(Yii::t('hipanel', 'Delete'), 'delete', 'danger'); ?>
+        <?php $page->endContent() ?>
+
+        <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+            <?= BillGridView::widget([
+                'boxed' => false,
+                'dataProvider' => $dataProvider,
+                'filterModel'  => $model,
+                'columns'      => [
+                    'checkbox', 'client_id', 'time', 'sum', 'balance',
+                    'type_label', 'description',
+                ],
+            ]) ?>
+        <?php $page->endBulkForm() ?>
+        <?php $page->endContent() ?>
+    <?php $page->end() ?>
+<?php Pjax::end() ?>
