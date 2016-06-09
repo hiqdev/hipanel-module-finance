cc9eb10d3945e9cc94055cf367e010112e059aee
tofid <andreyklochok@gmail.com>
2016-06-09 18:44:13 +0300

    Switch to IndexPage layout

--- src/views/tariff/_search.php
+++ src/views/tariff/_search.php
@@ -11,3 +11,19 @@
 /**
  * @var \hipanel\widgets\AdvancedSearch $search
  */
+use hipanel\modules\client\widgets\combo\ClientCombo;
+use hipanel\modules\client\widgets\combo\SellerCombo;
+
+?>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('tariff') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('note') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('client_id')->widget(ClientCombo::classname(), ['formElementSelector' => '.form-group']) ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('seller_id')->widget(SellerCombo::classname(), ['formElementSelector' => '.form-group']) ?>
+</div>
--- src/views/tariff/index.php
+++ src/views/tariff/index.php
@@ -1,68 +1,81 @@
 <?php
 
 use hipanel\modules\finance\grid\TariffGridView;
-use hipanel\widgets\ActionBox;
+use hipanel\widgets\IndexLayoutSwitcher;
+use hipanel\widgets\IndexPage;
+use hipanel\widgets\Pjax;
+use yii\bootstrap\Dropdown;
 
-$this->title                   = Yii::t('app', 'Tariffs');
+$this->title = Yii::t('app', 'Tariffs');
 $this->subtitle = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<?php $box = ActionBox::begin(['model' => $model, 'dataProvider' => $dataProvider, 'options' => ['class' => 'box-info']]) ?>
-    <?php $box->beginActions() ?>
-    <div class="dropdown">
-        <?php if (Yii::$app->user->can('manage')) : ?>
-            <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                <?= Yii::t('app', 'Create tariff'); ?>&nbsp;
-                <span class="caret"></span>
-            </button>
-        <?php endif; ?>
-        <?= $box->renderSearchButton(); ?>
-        <?= $box->renderSorter([
-            'attributes' => [
-                'seller',
-                'client',
-                'tariff',
-            ],
-        ]) ?>
-        <?= $box->renderPerPage() ?>
-        <?php if (Yii::$app->user->can('manage')) : ?>
-            <?= \yii\bootstrap\Dropdown::widget([
-                'items' => [
-                    ['label' => Yii::t('app', 'Create tariff for domain(s)'), 'url' => '#'],
-                    ['label' => Yii::t('app', 'Create svds tariff'), 'url' => '#'],
-                    ['label' => Yii::t('app', 'Create ovds tariff'), 'url' => '#'],
-                    ['label' => Yii::t('app', 'Create server tariff'), 'url' => '#'],
-                    ['label' => Yii::t('app', 'Create resources tariff'), 'url' => '#'],
-                ],
-            ]) . '&nbsp;'; ?>
-        <?php endif; ?>
-    </div>
-    <?php $box->endActions() ?>
-    <?php if (Yii::$app->user->can('manage')) : ?>
-        <?= $box->renderBulkActions([
-                'items' => [
-                    $box->renderDeleteButton(Yii::t('app', 'Delete')),
-                ],
-            ]); ?>
-        <?php endif; ?>
-    <?= $box->renderSearchForm(compact('paymentType')) ?>
 
-<?php $box->end() ?>
-    <?php $box->beginBulkForm() ?>
-        <?= tariffGridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel'  => $model,
-            'columns'      => Yii::$app->user->can('manage')
-                ? [
-                    'checkbox',
-                    'tariff', 'note', 'used',
-                    'client_id', 'seller_id',
-                ]
-                : [
-                    'tariff', 'note', 'used',
-                    'client_id', 'seller_id',
-                ],
-        ]) ?>
-    <?php $box->endBulkForm(); ?>
+<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
+<?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
+
+<?= $page->setSearchFormData(compact(['paymentType'])) ?>
+
+<?php $page->beginContent('main-actions') ?>
+<?php if (Yii::$app->user->can('manage')) : ?>
+    <div class="dropdown">
+        <a class="btn btn-sm btn-success dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
+           aria-haspopup="true" aria-expanded="true">
+            <?= Yii::t('hipanel', 'Create'); ?>
+            <span class="caret"></span>
+        </a>
+        <?= Dropdown::widget([
+            'items' => [
+                ['label' => Yii::t('app', 'Create tariff for domain(s)'), 'url' => '#'],
+                ['label' => Yii::t('app', 'Create svds tariff'), 'url' => '#'],
+                ['label' => Yii::t('app', 'Create ovds tariff'), 'url' => '#'],
+                ['label' => Yii::t('app', 'Create server tariff'), 'url' => '#'],
+                ['label' => Yii::t('app', 'Create resources tariff'), 'url' => '#'],
+            ],
+        ]); ?>
+    </div>
+<?php endif; ?>
+<?php $page->endContent() ?>
+
+<?php $page->beginContent('show-actions') ?>
+<?= IndexLayoutSwitcher::widget() ?>
+
+<?= $page->renderSorter([
+    'attributes' => [
+        'seller',
+        'client',
+        'tariff',
+    ],
+]) ?>
+<?= $page->renderPerPage() ?>
+<?php $page->endContent() ?>
+
+<?php $page->beginContent('bulk-actions') ?>
+<?php if (Yii::$app->user->can('manage')) : ?>
+    <?= $page->renderBulkButton(Yii::t('app', 'Delete'), 'delete', 'danger'); ?>
+<?php endif; ?>
+<?php $page->endContent() ?>
+
+<?php $page->beginContent('table') ?>
+<?php $page->beginBulkForm() ?>
+<?= TariffGridView::widget([
+    'boxed' => false,
+    'dataProvider' => $dataProvider,
+    'filterModel' => $model,
+    'columns' => Yii::$app->user->can('manage')
+        ? [
+            'checkbox',
+            'tariff', 'note', 'used',
+            'client_id', 'seller_id',
+        ]
+        : [
+            'tariff', 'note', 'used',
+            'client_id', 'seller_id',
+        ],
+]) ?>
+<?php $page->endBulkForm() ?>
+<?php $page->endContent() ?>
+<?php $page->end() ?>
+<?php Pjax::end() ?>
