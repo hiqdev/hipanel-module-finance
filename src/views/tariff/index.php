e3a0660ec9176a60d2bbd8588d67c87cd9aa0459
Andrii Vasyliev <sol@hiqdev.com>
2016-08-24 08:04:21 +0000

    redone subtitle to original Yii style

--- src/views/tariff/index.php
+++ src/views/tariff/index.php
@@ -1,81 +1,77 @@
 <?php
 
 use hipanel\modules\finance\grid\TariffGridView;
-use hipanel\widgets\IndexLayoutSwitcher;
 use hipanel\widgets\IndexPage;
 use hipanel\widgets\Pjax;
 use yii\bootstrap\Dropdown;
 
 $this->title = Yii::t('hipanel', 'Tariffs');
-$this->subtitle = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
+$this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-
 <?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
 <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
 
-<?= $page->setSearchFormData(compact(['paymentType'])) ?>
+    <?= $page->setSearchFormData(compact(['paymentType'])) ?>
 
-<?php $page->beginContent('main-actions') ?>
-<?php if (Yii::$app->user->can('manage')) : ?>
-    <div class="dropdown">
-        <a class="btn btn-sm btn-success dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
-           aria-haspopup="true" aria-expanded="true">
-            <?= Yii::t('hipanel', 'Create'); ?>
-            <span class="caret"></span>
-        </a>
-        <?= Dropdown::widget([
-            'items' => [
-                ['label' => Yii::t('hipanel/finance/tariff', 'Create domain tariff'), 'url' => '#'],
-                ['label' => Yii::t('hipanel/finance/tariff', 'Create svds tariff'), 'url' => '#'],
-                ['label' => Yii::t('hipanel/finance/tariff', 'Create ovds tariff'), 'url' => '#'],
-                ['label' => Yii::t('hipanel/finance/tariff', 'Create server tariff'), 'url' => '#'],
-                ['label' => Yii::t('hipanel/finance/tariff', 'Create resources tariff'), 'url' => '#'],
+    <?php $page->beginContent('main-actions') ?>
+        <?php if (Yii::$app->user->can('manage')) : ?>
+            <div class="dropdown">
+                <a class="btn btn-sm btn-success dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="true">
+                    <?= Yii::t('hipanel', 'Create') ?>
+                    <span class="caret"></span>
+                </a>
+                <?= Dropdown::widget([
+                    'items' => [
+                        ['label' => Yii::t('hipanel/finance/tariff', 'Create domain tariff'), 'url' => '#'],
+                        ['label' => Yii::t('hipanel/finance/tariff', 'Create svds tariff'), 'url' => '#'],
+                        ['label' => Yii::t('hipanel/finance/tariff', 'Create ovds tariff'), 'url' => '#'],
+                        ['label' => Yii::t('hipanel/finance/tariff', 'Create server tariff'), 'url' => '#'],
+                        ['label' => Yii::t('hipanel/finance/tariff', 'Create resources tariff'), 'url' => '#'],
+                    ],
+                ]) ?>
+            </div>
+        <?php endif ?>
+    <?php $page->endContent() ?>
+
+    <?php $page->beginContent('show-actions') ?>
+        <?= $page->renderLayoutSwitcher() ?>
+        <?= $page->renderSorter([
+            'attributes' => [
+                'seller', 'client',
+                'tariff',
             ],
-        ]); ?>
-    </div>
-<?php endif; ?>
-<?php $page->endContent() ?>
+        ]) ?>
+        <?= $page->renderPerPage() ?>
+    <?php $page->endContent() ?>
 
-<?php $page->beginContent('show-actions') ?>
-<?= IndexLayoutSwitcher::widget() ?>
+    <?php $page->beginContent('bulk-actions') ?>
+        <?php if (Yii::$app->user->can('manage')) : ?>
+            <?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), 'delete', 'danger') ?>
+        <?php endif ?>
+    <?php $page->endContent() ?>
 
-<?= $page->renderSorter([
-    'attributes' => [
-        'seller',
-        'client',
-        'tariff',
-    ],
-]) ?>
-<?= $page->renderPerPage() ?>
-<?php $page->endContent() ?>
-
-<?php $page->beginContent('bulk-actions') ?>
-<?php if (Yii::$app->user->can('manage')) : ?>
-    <?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), 'delete', 'danger'); ?>
-<?php endif; ?>
-<?php $page->endContent() ?>
-
-<?php $page->beginContent('table') ?>
-<?php $page->beginBulkForm() ?>
-<?= TariffGridView::widget([
-    'boxed' => false,
-    'dataProvider' => $dataProvider,
-    'filterModel' => $model,
-    'columns' => Yii::$app->user->can('manage')
-        ? [
-            'checkbox',
-            'tariff', 'note', 'used',
-            'client_id', 'seller_id',
-        ]
-        : [
-            'tariff', 'note', 'used',
-            'client_id', 'seller_id',
-        ],
-]) ?>
-<?php $page->endBulkForm() ?>
-<?php $page->endContent() ?>
+    <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+        <?= TariffGridView::widget([
+            'boxed' => false,
+            'dataProvider' => $dataProvider,
+            'filterModel' => $model,
+            'columns' => Yii::$app->user->can('manage')
+                ? [
+                    'checkbox',
+                    'tariff', 'note', 'used',
+                    'client_id', 'seller_id',
+                ]
+                : [
+                    'tariff', 'note', 'used',
+                    'client_id', 'seller_id',
+                ],
+        ]) ?>
+        <?php $page->endBulkForm() ?>
+    <?php $page->endContent() ?>
 <?php $page->end() ?>
 <?php Pjax::end() ?>
