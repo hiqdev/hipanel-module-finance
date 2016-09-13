5b6f4190f6dc59baa8ac29ed0a8c050dc370f8e2
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-09-13 19:34:31 +0300

    Added cart/Calculator basic model

---
+++ src/cart/Calculation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace hipanel\modules\finance\cart;
+
+class Calculation extends \hipanel\modules\finance\models\Calculation
+{
+    /**
+     * @var AbstractCartPosition
+     */
+    public $position;
+
+    /** {@inheritdoc} */
+    public static function primaryKey()
+    {
+        return ['cart_position_id'];
+    }
+
+    public function synchronize()
+    {
+        $this->cart_position_id = $this->position->getId();
+        $this->amount = $this->position->getQuantity();
+    }
+
+    public function rules()
+    {
+        return array_merge(parent::rules(), [
+            [['cart_position_id'], 'safe']
+        ]);
+    }
+}
