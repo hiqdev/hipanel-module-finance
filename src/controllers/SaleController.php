e007e25f88834d9e99228d3cbbde8a837e42356a
tofid <andreyklochok@gmail.com>
2017-05-15 19:09:11 +0300

    Added view action to SaleController

--- src/controllers/SaleController.php
+++ src/controllers/SaleController.php
@@ -3,6 +3,11 @@
 namespace hipanel\modules\finance\controllers;
 
 use hipanel\actions\IndexAction;
+use hipanel\actions\ViewAction;
+use hipanel\modules\client\models\stub\ClientRelationFreeStub;
+use hipanel\modules\finance\logic\TariffManagerFactory;
+use hipanel\modules\finance\models\Tariff;
+use yii\base\Event;
 
 class SaleController extends \hipanel\base\CrudController
 {
@@ -11,6 +16,22 @@ class SaleController extends \hipanel\base\CrudController
         return [
             'index' => [
                 'class' => IndexAction::class,
+            ],
+            'view' => [
+                'class' => ViewAction::class,
+                'data' => function ($action) {
+                    $sale = $action->model;
+                    $attributes = [
+                        'id' => $sale->buyer_id,
+                        'login' => $sale->buyer,
+                        'seller' => $sale->seller,
+                        'seller_id' => $sale->seller_id,
+                    ];
+                    $client = new ClientRelationFreeStub($attributes);
+                    $tariff = Tariff::find()->where(['id' => $sale->tariff_id])->one();
+
+                    return compact('client', 'tariff');
+                },
             ],
         ];
     }
