8d087f0688964eecdc11eba0b584ac1e229c2adb
tofid <andreyklochok@gmail.com>
2017-06-07 19:33:21 +0300

    Doing Bill view

--- src/views/bill/view.php
+++ src/views/bill/view.php
@@ -1,20 +1,27 @@
 <?php
 
-use hipanel\helpers\Url;
 use hipanel\modules\finance\grid\BillGridView;
+use hipanel\modules\finance\menus\BillDetailMenu;
+use hipanel\widgets\ClientSellerLink;
+use hipanel\widgets\MainDetails;
 use hipanel\widgets\Pjax;
 use yii\helpers\Html;
 
-$this->title = Html::encode($model->domain);
+$this->title = $model->label;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel:finance', 'Payments'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
-
-Yii::$app->response->redirect(Url::to('@bill/index'))->send();
 
 Pjax::begin(Yii::$app->params['pjax']) ?>
 <div class="row">
 
-    <div class="col-md-4">
+    <div class="col-md-3">
+        <?= MainDetails::widget([
+            'title' => $model->gtype_label,
+            'icon' => 'fa-money',
+//            'subTitle' => Html::a($model->client, ['@client/view', 'id' => $model->client_id]),
+            'subTitle' => ClientSellerLink::widget(['model' => $model]),
+            'menu' => BillDetailMenu::widget(['model' => $model], ['linkTemplate' => '<a href="{url}" {linkOptions}><span class="pull-right">{icon}</span>&nbsp;{label}</a>']),
+        ]) ?>
         <?= BillGridView::detailView([
             'model' => $model,
             'columns' => [
