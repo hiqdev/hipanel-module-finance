137830f0a1ed8d32d2bdaa8d289f56d553540299
tofid <andreyklochok@gmail.com>
2017-06-07 19:32:32 +0300

    Added menus for Bill

---
+++ src/menus/BillActionsMenu.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace hipanel\modules\finance\menus;
+
+use Yii;
+
+class BillActionsMenu extends \hiqdev\yii2\menus\Menu
+{
+    public $model;
+
+    public function items()
+    {
+        return [
+            'view' => [
+                'label' => Yii::t('hipanel', 'View'),
+                'icon' => 'fa-info',
+                'url' => ['@bill/view', 'id' => $this->model->id],
+            ],
+            'copy' => [
+                'label' => Yii::t('hipanel', 'Copy'),
+                'icon' => 'fa-copy',
+                'url' => ['@bill/copy', 'id' => $this->model->id],
+            ],
+            'update' => [
+                'label' => Yii::t('hipanel', 'Update'),
+                'icon' => 'fa-pencil',
+                'url' => ['@bill/update', 'id' => $this->model->id],
+            ],
+        ];
+    }
+}
---
+++ src/menus/BillDetailMenu.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace hipanel\modules\finance\menus;
+
+use Yii;
+
+class BillDetailMenu extends \hipanel\menus\AbstractDetailMenu
+{
+    public $model;
+
+    public function items()
+    {
+        $actions = BillActionsMenu::create(['model' => $this->model])->items();
+        $items = array_merge($actions, [
+            'delete' => [
+                'label' => Yii::t('hipanel', 'Delete'),
+                'icon' => 'fa-trash',
+                'url' => ['@bill/view', 'id' => $this->model->id],
+                'encode' => false,
+                'linkOptions' => [
+                    'data' => [
+                        'confirm' => Yii::t('hipanel', 'Are you sure you want to delete this item?'),
+                        'method' => 'POST',
+                        'pjax' => '0',
+                    ],
+                ],
+            ],
+        ]);
+        unset($items['view']);
+
+        return $items;
+    }
+}
