88cc8edd0c7b2e27937f1a663bccd0709113d65c
tofid <andreyklochok@gmail.com>
2017-07-05 20:04:19 +0300

    Fixed ActionMenu sticky popover in Bill index page

--- src/menus/BillActionsMenu.php
+++ src/menus/BillActionsMenu.php
@@ -15,18 +15,27 @@ class BillActionsMenu extends \hiqdev\yii2\menus\Menu
                 'label' => Yii::t('hipanel', 'View'),
                 'icon' => 'fa-info',
                 'url' => ['@bill/view', 'id' => $this->model->id],
+                'linkOptions' => [
+                    'data-pjax' => 0,
+                ],
             ],
             'copy' => [
                 'label' => Yii::t('hipanel', 'Copy'),
                 'icon' => 'fa-copy',
                 'url' => ['@bill/copy', 'id' => $this->model->id],
                 'visible' => Yii::$app->user->can('bill.create'),
+                'linkOptions' => [
+                    'data-pjax' => 0,
+                ],
             ],
             'update' => [
                 'label' => Yii::t('hipanel', 'Update'),
                 'icon' => 'fa-pencil',
                 'url' => ['@bill/update', 'id' => $this->model->id],
                 'visible' => Yii::$app->user->can('bill.update'),
+                'linkOptions' => [
+                    'data-pjax' => 0,
+                ],
             ],
         ];
     }
