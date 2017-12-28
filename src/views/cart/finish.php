7e9cd07bc44d02b50ce54fe88de469daad023baa
tofid <andreyklochok@gmail.com>
2017-12-28 15:46:21 +0200

    Fixed html marckup for cart/finish page

--- src/views/cart/finish.php
+++ src/views/cart/finish.php
@@ -14,114 +14,127 @@ $this->title = Yii::t('cart', 'Order execution');
 ?>
 
 <?php if (!empty($error)) : ?>
-    <div class="box box-danger box-solid">
-        <div class="box-header with-border">
-            <?= Html::tag('h3', '<i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;' . Yii::t('cart', 'Operations failed') . ': ' . Yii::t('cart', '{0, plural, one{# position} other{# positions}}', count($error))); ?>
-        </div>
-        <div class="box-body">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th class="text-center">#</th>
-                    <th><?= Yii::t('cart', 'Description') ?></th>
-                    <th class="text-right"><?= Yii::t('cart', 'Price') ?></th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $no = 1;
-                foreach ($error as $exception) :
-                    $item = $exception->position;
-                    ?>
-                    <tr>
-                        <td class="text-center text-bold"><?= $no++ ?></td>
-                        <td>
-                            <?= $item->renderDescription() ?><br>
-                            <?= $exception->getMessage() ?>
-                        </td>
-                        <td align="right" class="text-bold"><?= Yii::$app->formatter->asCurrency($item->cost, Yii::$app->params['currency']) ?></td>
-                        <td></td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-danger box-solid">
+                <div class="box-header with-border">
+                    <?= Html::tag('h3', '<i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;' . Yii::t('cart', 'Operations failed') . ': ' . Yii::t('cart', '{0, plural, one{# position} other{# positions}}', count($error))); ?>
+                </div>
+                <div class="box-body">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th class="text-center">#</th>
+                            <th><?= Yii::t('cart', 'Description') ?></th>
+                            <th class="text-right"><?= Yii::t('cart', 'Price') ?></th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $no = 1;
+                        foreach ($error as $exception) :
+                            $item = $exception->position;
+                            ?>
+                            <tr>
+                                <td class="text-center text-bold"><?= $no++ ?></td>
+                                <td>
+                                    <?= $item->renderDescription() ?><br>
+                                    <?= $exception->getMessage() ?>
+                                </td>
+                                <td align="right" class="text-bold"><?= Yii::$app->formatter->asCurrency($item->cost, Yii::$app->params['currency']) ?></td>
+                                <td></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 <?php endif; ?>
 
 
 <?php if (!empty($pending)) : ?>
-    <div class="box box-warning box-solid">
-        <div class="box-header with-border">
-            <?= Html::tag('h3', '<i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;' . Yii::t('cart', 'Pending operations') . ': ' . Yii::t('cart', '{0, plural, one{# position} other{# positions}}', count($pending))); ?>
-        </div>
-        <div class="box-body">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th class="text-center">#</th>
-                    <th><?= Yii::t('cart', 'Description') ?></th>
-                    <th class="text-right"><?= Yii::t('cart', 'Price') ?></th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $no = 1;
-                foreach ($pending as $exception) :
-                    $item = $exception->position;
-                    ?>
-                    <tr>
-                        <td class="text-center text-bold"><?= $no++ ?></td>
-                        <td>
-                            <?= $item->renderDescription() ?><br>
-                            <?= $exception->getMessage() ?>
-                        </td>
-                        <td align="right" class="text-bold"><?= Yii::$app->formatter->asCurrency($item->cost, Yii::$app->params['currency']) ?></td>
-                        <td></td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-warning box-solid">
+                <div class="box-header with-border">
+                    <?= Html::tag('h3', '<i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;' . Yii::t('cart', 'Pending operations') . ': ' . Yii::t('cart', '{0, plural, one{# position} other{# positions}}', count($pending))); ?>
+                </div>
+                <div class="box-body">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th class="text-center">#</th>
+                            <th><?= Yii::t('cart', 'Description') ?></th>
+                            <th class="text-right"><?= Yii::t('cart', 'Price') ?></th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $no = 1;
+                        foreach ($pending as $exception) :
+                            $item = $exception->position;
+                            ?>
+                            <tr>
+                                <td class="text-center text-bold"><?= $no++ ?></td>
+                                <td>
+                                    <?= $item->renderDescription() ?><br>
+                                    <?= $exception->getMessage() ?>
+                                </td>
+                                <td align="right" class="text-bold"><?= Yii::$app->formatter->asCurrency($item->cost, Yii::$app->params['currency']) ?></td>
+                                <td></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 <?php endif; ?>
 
 <?php if (count($success)) : ?>
-<div class="box box-success">
-    <div class="box-header with-border">
-        <h3 class="box-title">
-            <?= Yii::t('cart', 'Operations performed') ?>: <?= Yii::t('cart', '{0, plural, one{# position} other{# positions}}', count($success)) ?>
-        </h3>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <h3 class="box-title">
+                        <?= Yii::t('cart', 'Operations performed') ?>
+                        : <?= Yii::t('cart', '{0, plural, one{# position} other{# positions}}', count($success)) ?>
+                    </h3>
+                </div>
+                <div class="box-body">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th class="text-center">#</th>
+                            <th><?= Yii::t('cart', 'Description') ?></th>
+                            <th><?= Yii::t('cart', 'Notes') ?></th>
+                            <th class="text-right"><?= Yii::t('cart', 'Price') ?></th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php $no = 1 ?>
+                        <?php foreach ($success as $purchase) : ?>
+                            <?php $item = $purchase->position ?>
+                            <tr>
+                                <td class="text-center text-bold"><?= $no++ ?></td>
+                                <td><?= $item->renderDescription() ?></td>
+                                <td><?= $purchase->renderNotes() ?></td>
+                                <td align="right" class="text-bold"><?= Yii::$app->formatter->asCurrency($item->cost, Yii::$app->params['currency']) ?></td>
+                                <td></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="box-body">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th class="text-center">#</th>
-                <th><?= Yii::t('cart', 'Description') ?></th>
-                <th><?= Yii::t('cart', 'Notes') ?></th>
-                <th class="text-right"><?= Yii::t('cart', 'Price') ?></th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php $no = 1 ?>
-            <?php foreach ($success as $purchase) : ?>
-                <?php $item = $purchase->position ?>
-                <tr>
-                    <td class="text-center text-bold"><?= $no++ ?></td>
-                    <td><?= $item->renderDescription() ?></td>
-                    <td><?= $purchase->renderNotes() ?></td>
-                    <td align="right" class="text-bold"><?= Yii::$app->formatter->asCurrency($item->cost, Yii::$app->params['currency']) ?></td>
-                    <td></td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-</div>
 <?php endif ?>
 
 <?php if (count($remarks)) : ?>
