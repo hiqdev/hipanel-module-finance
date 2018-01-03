c060b6706fa817ee539644cf064cec230a3f62e0
tofid <andreyklochok@gmail.com>
2018-01-03 17:03:27 +0200

    minor

--- src/views/plan/_search.php
+++ src/views/plan/_search.php
@@ -7,6 +7,10 @@ use hipanel\modules\client\widgets\combo\ClientCombo;
 ?>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('name') ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('client_id')->widget(ClientCombo::class) ?>
 </div>
 
