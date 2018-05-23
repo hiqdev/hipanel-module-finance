ca660b53a388355967b30cf4ab63cdc81d19f80c
Andrey Klochok <andreyklochok@gmail.com>
2018-05-23 19:20:13 +0300

    Fixed search by deleted state

--- src/models/PlanSearch.php
+++ src/models/PlanSearch.php
@@ -3,10 +3,18 @@
 namespace hipanel\modules\finance\models;
 
 use hipanel\base\SearchModelTrait;
+use yii\helpers\ArrayHelper;
 
 class PlanSearch extends Plan
 {
     use SearchModelTrait {
         searchAttributes as defaultSearchAttributes;
+    }
+
+    public function searchAttributes()
+    {
+        return ArrayHelper::merge($this->defaultSearchAttributes(), [
+            'states',
+        ]);
     }
 }
