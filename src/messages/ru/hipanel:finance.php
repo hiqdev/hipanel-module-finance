b6aaec9b2606771f5b5ec778cedf4041cbfccf1e
Andrii Vasyliev <sol@hiqdev.com>
2018-06-14 16:39:18 +0000

    added `ItemsQuantity` for hw_purchase

--- src/messages/ru/hipanel:finance.php
+++ src/messages/ru/hipanel:finance.php
@@ -53,6 +53,7 @@ return [
     'Payment system' => 'Платёжная система',
     'TXN' => 'Транзакция',
     'Add payment' => 'Создать платёж',
+    '{quantity, plural, one{# item} other{# items}}' => '{quantity, plural, one{# штука} few{# штуки} other{# штук}}',
     '{quantity, plural, one{# minute} other{# minutes}}' => '{quantity, plural, one{# минута} few{# минуты} other{# минут}}',
     '{quantity, plural, one{# day} other{# days}}' => '{quantity, plural, one{# день} few{# дня} other{# дней}}',
     '{quantity, plural, one{# year} other{# years}}' => '{quantity, plural, one{# год} few{# года} other{# лет}}',
