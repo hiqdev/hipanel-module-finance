3048ca14140a5b2a2ebc19805d600c47cbd570e4
Yurii Myronchuk <bladeroot@gmail.com>
2018-07-10 23:46:03 +0300

    rewrite widget (#23)
    * rewrite widget
    
    * rename variable

--- src/widgets/AvailableMerchants.php
+++ src/widgets/AvailableMerchants.php
@@ -4,15 +4,33 @@ namespace hipanel\modules\finance\widgets;
 
 use yii\base\Widget;
 use yii\helpers\Html;
+use Yii;
 
 class AvailableMerchants extends Widget
 {
-    public $merchants = [];
+    public $merchants = null;
+    const PAYPAL_PROCESSING = 'PayPal';
 
     public function run()
     {
-        $list = array_map(function ($merchant) { return Html::tag('b', $merchant->label); }, $this->merchants);
+        if ($this->merchants === null && Yii::$app->hasModule('merchant')) {
+            $this->merchants = Yii::$app->getModule('merchant')->getPurchaseRequestCollection()->getItems();
+        }
 
-        return implode(',&nbsp; ', array_unique($list));
+        if (empty($this->merchants)) {
+            return '';
+        }
+
+        $list = array_map(function($merchant) { return $merchant->label; }, $this->merchants);
+        $htmlList = array_map(function($merchant) { return Html::tag('b', $merchant); }, $list);
+        $htmlList = array_unique($htmlList);
+
+        $out[] = Yii::t('hipanel:finance', 'We accept the following automatic payment methods') . ":";
+        $out[] = implode(',&nbsp; ', $htmlList);
+        if (in_array(self::PAYPAL_PROCESSING, $list, true)) {
+            $out[] = Yii::t('hipanel:finance', 'as well as PayPal payments from your Visa and MasterCard');
+        }
+
+        return implode(" ", $out);
     }
 }
