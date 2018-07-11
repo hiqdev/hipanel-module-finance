b6655699f485a8ae983fa34c1ef9239567f3e1a1
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-07-11 18:34:53 +0300

    Fixed AvailableMerchants to work only when client is allowed to deposit

--- src/widgets/AvailableMerchants.php
+++ src/widgets/AvailableMerchants.php
@@ -2,18 +2,23 @@
 
 namespace hipanel\modules\finance\widgets;
 
+use hipanel\modules\finance\models\Merchant;
 use yii\base\Widget;
 use yii\helpers\Html;
 use Yii;
 
 class AvailableMerchants extends Widget
 {
-    public $merchants = null;
+    /**
+     * @var Merchant[]
+     */
+    public $merchants;
+
     const PAYPAL_PROCESSING = 'PayPal';
 
     public function run()
     {
-        if ($this->merchants === null && Yii::$app->hasModule('merchant')) {
+        if ($this->merchants === null && Yii::$app->hasModule('merchant') && Yii::$app->user->can('deposit')) {
             $this->merchants = Yii::$app->getModule('merchant')->getPurchaseRequestCollection()->getItems();
         }
 
@@ -22,12 +27,11 @@ class AvailableMerchants extends Widget
         }
 
         $list = array_map(function($merchant) { return $merchant->label; }, $this->merchants);
-        $htmlList = array_map(function($merchant) { return Html::tag('b', $merchant); }, $list);
-        $htmlList = array_unique($htmlList);
+        $htmlList = array_unique(array_map(function($merchant) { return Html::tag('b', $merchant); }, $list));
 
         $out[] = Yii::t('hipanel:finance', 'We accept the following automatic payment methods') . ":";
         $out[] = implode(',&nbsp; ', $htmlList);
-        if (in_array(self::PAYPAL_PROCESSING, $list, true)) {
+        if (\in_array(self::PAYPAL_PROCESSING, $list, true)) {
             $out[] = Yii::t('hipanel:finance', 'as well as PayPal payments from your Visa and MasterCard');
         }
 
