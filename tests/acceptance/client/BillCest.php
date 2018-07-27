92fc11f80f1ba325d41d4833e03e527729f2b15c
Pavlo <pkolomiy@gmail.com>
2018-07-27 12:31:20 +0300

    Added BillCest and AccountRechargingCest (#26)
    * Added BillCest and AccountRechargingCest
    
    * Changed BillCest using GridView helper
    
    * Changed GridView call
    
    * Changed BillCest using IndexPage
    
    * Changed BillCest according to suggestions

---
+++ tests/acceptance/client/BillCest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace hipanel\modules\finance\tests\acceptance\client;
+
+use hipanel\helpers\Url;
+use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Step\Acceptance\Client;
+
+class BillCest
+{
+    public function ensureIndexPageWorks(Client $I)
+    {
+        $I->login();
+        $I->needPage(Url::to('@bill/index'));
+        $I->see('Bills', 'h1');
+        $this->ensureICanSeeAdvancedSearchBox($I);
+        $this->ensureICanSeeBulkBillSearchBox($I);
+    }
+
+    private function ensureICanSeeAdvancedSearchBox(Client $I)
+    {
+        $I->see('Advanced search');
+        $I->seeLink('Recharge account', Url::to('@pay/deposit'));
+
+        $index = new IndexPage($I);
+        $index->containsFilters('form-advancedsearch-bill-search', [
+            ['input' => ['placeholder' => 'Currency']],
+            ['input' => [
+                'id' => 'billsearch-time_from',
+                'name' => 'date-picker',
+            ]],
+            ['input' => ['placeholder' => 'Type']],
+            ['input' => ['placeholder' => 'Servers']],
+            ['input' => ['placeholder' => 'Description']],
+        ]);
+
+        $I->see('Date', 'label');
+        $I->see('Tariff', 'span');
+    }
+
+    private function ensureICanSeeBulkBillSearchBox(Client $I)
+    {
+        $index = new IndexPage($I);
+        $index->containsColumns('bulk-bill-search', [
+            'Time',
+            'Sum',
+            'Balance',
+            'Type',
+            'Description',
+        ]);
+    }
+}
