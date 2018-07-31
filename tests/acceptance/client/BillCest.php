ca88cd0d04b7d408ebceaacc9fb3fbc5344791d4
pkolomiy <pkolomiy@gmail.com>
2018-07-31 18:05:49 +0300

    Changed tests according to changes in IndexPage in hipanel-core

--- tests/acceptance/client/BillCest.php
+++ tests/acceptance/client/BillCest.php
@@ -4,44 +4,46 @@ namespace hipanel\modules\finance\tests\acceptance\client;
 
 use hipanel\helpers\Url;
 use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Page\Widget\Input\Input;
+use hipanel\tests\_support\Page\Widget\Input\Select2;
 use hipanel\tests\_support\Step\Acceptance\Client;
 
 class BillCest
 {
+    /**
+     * @var IndexPage
+     */
+    private $index;
+
+    public function _before(Client $I)
+    {
+        $this->index = new IndexPage($I);
+    }
+
     public function ensureIndexPageWorks(Client $I)
     {
         $I->login();
         $I->needPage(Url::to('@bill/index'));
         $I->see('Bills', 'h1');
-        $this->ensureICanSeeAdvancedSearchBox($I);
-        $this->ensureICanSeeBulkBillSearchBox($I);
-    }
-
-    private function ensureICanSeeAdvancedSearchBox(Client $I)
-    {
-        $I->see('Advanced search');
         $I->seeLink('Recharge account', Url::to('@pay/deposit'));
-
-        $index = new IndexPage($I);
-        $index->containsFilters('form-advancedsearch-bill-search', [
-            ['input' => ['placeholder' => 'Currency']],
-            ['input' => [
-                'id' => 'billsearch-time_from',
-                'name' => 'date-picker',
-            ]],
-            ['input' => ['placeholder' => 'Type']],
-            ['input' => ['placeholder' => 'Servers']],
-            ['input' => ['placeholder' => 'Description']],
-        ]);
-
-        $I->see('Date', 'label');
-        $I->see('Tariff', 'span');
+        $this->ensureICanSeeAdvancedSearchBox();
+        $this->ensureICanSeeBulkBillSearchBox();
     }
 
-    private function ensureICanSeeBulkBillSearchBox(Client $I)
+    private function ensureICanSeeAdvancedSearchBox()
     {
-        $index = new IndexPage($I);
-        $index->containsColumns('bulk-bill-search', [
+        $this->index->containsFilters([
+            new Input('Currency'),
+            new Input('Type'),
+            new Input('Servers'),
+            new Input('Description'),
+            new Select2('Tariff'),
+        ]);
+    }
+
+    private function ensureICanSeeBulkBillSearchBox()
+    {
+        $this->index->containsColumns([
             'Time',
             'Sum',
             'Balance',
