2191509c38e76b8a3d84be0453d62d82ec14cc23
Dmytro Naumenko <d.naumenko.a@gmail.com>
2018-11-09 18:18:35 +0200

    fixed codestyle
    Co-Authored-By: omentes <setnemo@gmail.com>

--- tests/_support/Page/plan/ViewGrouping.php
+++ tests/_support/Page/plan/ViewGrouping.php
@@ -7,6 +7,6 @@ class ViewGrouping extends View
     public function seePlan()
     {
         parent::seePlan();
-        $I->see("Grouping", "//span[contains(text(), 'Grouping')]");
+        $I->see('Grouping', '//span');
     }
 }
