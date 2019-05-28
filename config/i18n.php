ea6ad94a8fcd35e7ee178484502cb5090b7bcbc2
Andrii Vasyliev <sol@hiqdev.com>
2019-05-28 10:33:00 +0000

    Moved config dir to root, added params

---
+++ config/i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Finance module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-finance
+ * @package   hipanel-module-finance
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2019, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'languages' => ['ru'],
+    'sourcePath' => dirname(__DIR__) . '/src',
+    'messagePath' => dirname(__DIR__) . '/src/messages',
+    'sort' => true,
+    'removeUnused' => true,
+];
