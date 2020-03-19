2d2d66786f0550c4a87c5f407c802e5ab8c5b46d
Andrey Klochok <andreyklochok@gmail.com>
2020-03-19 19:15:05 +0200

    Moved Relate positions from yii2-cart (#145)
    * Moved Relate positions from yii2-cart
    
    * minor

---
+++ src/cart/RelatedPosition.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace hipanel\modules\finance\cart;
+
+use hipanel\modules\finance\logic\Calculator;
+use hipanel\modules\finance\models\CalculableModelInterface;
+use hiqdev\yii2\cart\CartPositionInterface;
+use hiqdev\yii2\cart\Module;
+use hiqdev\yii2\cart\ShoppingCart;
+
+abstract class RelatedPosition implements RelatedPositionInterface
+{
+    /** @var CartPositionInterface */
+    public $mainPosition;
+
+    /** @var ShoppingCart */
+    public $cart;
+
+    public function __construct(CartPositionInterface $mainPosition)
+    {
+        $this->cart = Module::getInstance()->getCart();
+        $this->mainPosition = $mainPosition;
+    }
+
+    /** @inheritDoc */
+    public function render(): string
+    {
+        return $this->getWidget()->run();
+    }
+
+    public function calculate(CalculableModelInterface $position): CalculableModelInterface
+    {
+        $calculator = new Calculator([$position]);
+        $calculationId = $position->getCalculationModel()->calculation_id;
+        $calculation = $calculator->getCalculation($calculationId);
+        $value = $calculation->forCurrency($this->cart->getCurrency());
+        $position->setPrice($value->price);
+        $position->setValue($value->value);
+        $position->setCurrency($value->currency);
+
+        return $position;
+    }
+
+}
