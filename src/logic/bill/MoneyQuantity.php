7e39a8b45f966573308c5f58c4028b010d60aa89
Andrii Vasyliev <sol@hiqdev.com>
2020-07-20 17:10:45 +0000

    Add `MoneyQuantity` formatter for `referral` resources

---
+++ src/logic/bill/MoneyQuantity.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Finance module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-finance
+ * @package   hipanel-module-finance
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2019, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\finance\logic\bill;
+
+use Money\Formatter\DecimalMoneyFormatter;
+use Money\Money;
+use Money\Currency;
+use Yii;
+
+/**
+ * Class MoneyQuantity.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class MoneyQuantity extends DefaultQuantityFormatter
+{
+    public function format(): string
+    {
+        $formatter = Yii::$container->get(DecimalMoneyFormatter::class);
+        $sum = $formatter->format($this->getAmount());
+
+        return Yii::$app->formatter->asCurrency($sum, $this->getCurrency());
+    }
+
+    public function getAmount(): Money
+    {
+        $quantity = $this->getQuantity();
+        $amount = $quantity->getQuantity();
+
+        return new Money($amount, new Currency($this->getCurrency()));
+    }
+
+    public function getCurrency(): string
+    {
+        return strtoupper($this->getQuantity()->getUnit()->getName());
+    }
+}
