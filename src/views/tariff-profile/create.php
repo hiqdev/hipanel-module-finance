ae77362e9f6d9a543006152a1dbf1b5d449b33f3
Andrey Klochok <andreyklochok@gmail.com>
2021-02-12 16:31:56 +0200

    Add clarification on behalf of which seller creates/update the tariff profile

--- src/views/tariff-profile/create.php
+++ src/views/tariff-profile/create.php
@@ -1,6 +1,6 @@
 <?php
 
-$this->title = Yii::t('hipanel', 'Create');
+$this->title = Yii::t('hipanel.finance.tariffprofile', 'Create seller profile: {0}', Yii::$app->user->identity->login);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel.finance.tariffprofile', 'Tariff profiles'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
--- src/views/tariff-profile/update.php
+++ src/views/tariff-profile/update.php
@@ -1,6 +1,6 @@
 <?php
 
-$this->title = Yii::t('hipanel', 'Update');
+$this->title = Yii::t('hipanel.finance.tariffprofile', 'Update seller profile: {0}', Yii::$app->user->identity->login);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('hipanel.finance.tariffprofile', 'Tariff profiles'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = $this->title;
