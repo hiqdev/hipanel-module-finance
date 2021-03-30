f3cc2792fa744033b6c4fd658fe19b8061a6edd8
Andrey Klochok <andreyklochok@gmail.com>
2021-03-30 11:57:33 +0300

    Fix resource detail limits (#215)

--- src/actions/ResourceDetailAction.php
+++ src/actions/ResourceDetailAction.php
@@ -20,42 +20,24 @@ class ResourceDetailAction extends IndexAction
 
     public function init(): void
     {
-        $configurator = $this->configurator;
         if (!$this->data) {
             $this->data = fn(Action $action, $data): array => [
                 'originalModel' => call_user_func([$this->model, 'findOne'], $action->controller->request->get('id')),
             ];
         }
         $this->setSearchModel(new ResourceSearch());
-        $this->responseVariants['resource-total'] = static function (VariantsAction $action) use ($configurator): string {
-            $dataProvider = $action->parent->getDataProvider();
-            $resources = $dataProvider->getModels();
-            if (count($types = array_unique(ArrayHelper::getColumn($resources, 'type'))) > 1) {
-                return '--';
-            }
-            $gropedResources = ResourceHelper::aggregateByObject($resources, $configurator);
-            $total = ResourceHelper::calculateTotal($gropedResources);
-            $result = $total[reset($types)];
-            if (empty($result)) {
-                return '';
-            }
-
-            return Yii::t('hipanel:finance', '{qty} {unit}', ['qty' => $result['qty'], 'unit' => $result['unit']]);
-        };
         parent::init();
     }
 
     public function beforePerform()
     {
         parent::beforePerform();
-        $query = $this->getDataProvider()->query;
-        $query->andWhere([
+        $this->getDataProvider()->query->andWhere([
             'time_from' => (new DateTime('2007-01-01'))->format('Y-m-d'),
             'time_till' => (new DateTime())->modify('last day of this month')->format('Y-m-d'),
-        ]);
-        $query->andWhere([
             'object_id' => Yii::$app->request->get('id'),
             'groupby' => 'server_traf_year',
+            'limit' => 'ALL',
         ]);
     }
 }
