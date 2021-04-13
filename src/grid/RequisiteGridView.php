cad8b7673ec07c802ee976821c40f9a614e9d00f
Yurii Myronchuk <bladeroot@gmail.com>
2021-04-13 17:34:59 +0300

    Requisite balance (#224)
    * init
    
    * finish
    
    * csfix
    
    Co-authored-by: Yurii Myronchuk <bladeroot@hiqdev.com>

--- src/grid/RequisiteGridView.php
+++ src/grid/RequisiteGridView.php
@@ -12,14 +12,57 @@ namespace hipanel\modules\finance\grid;
 
 use hipanel\helpers\Url;
 use hipanel\modules\finance\menus\RequisiteActionsMenu;
+use hipanel\modules\finance\models\Requisite;
 use hiqdev\yii2\menus\grid\MenuColumn;
 use hipanel\modules\client\grid\ContactGridView;
 use hipanel\grid\XEditableColumn;
+use hipanel\models\Ref;
+use yii\helpers\Html;
+use Yii;
 
 class RequisiteGridView extends ContactGridView
 {
     public function columns()
     {
+        $currencies = Ref::getList('type,currency');
+        foreach ($currencies as $currency => $name) {
+            $curColumns[$currency] = [
+                'format' => 'raw',
+                'attribute' => 'balances',
+                'filter' => false,
+                'label' => Yii::t('hipanel:finance', strtoupper($currency)),
+                'value' => static function (Requisite $model) use ($currency): string {
+                    $tags[] = Html::tag('span', Yii::t('hipanel:finance', "Balance: {b}", [
+                        'b' => $model->balances[$currency]['balance'] ?? "0.00",
+                    ]), ['class' => 'label label-default']);
+                    $tags[] = Html::tag('span', Yii::t('hipanel:finance', "Debit: {b}", [
+                        'b' => $model->balances[$currency]['debit'] ?? "0.00",
+                    ]), ['class' => 'label label-primary']);
+                    $tags[] = Html::tag('span', Yii::t('hipanel:finance', "Credit: {b}", [
+                        'b' => $model->balances[$currency]['credit'] ?? "0.00",
+                    ]), ['class' => 'label label-success']);
+
+                    return implode("<br/>", $tags);
+                },
+            ];
+        }
+
+        foreach ([
+                'balance' => Yii::t('hipanel:finance', "Balance"),
+                'debit' => Yii::t('hipanel:finance', "Debit"),
+                'credit' => Yii::t('hipanel:finance', "Credit"),
+        ] as $attr => $name) {
+            $balanceColumns[$attr] = [
+                'format' => 'raw',
+                'attribute' => 'balance',
+                'filter' => false,
+                'label' => $name,
+                'value' => static function (Requisite $model) use ($attr): string {
+                    return Html::tag('span', $model->balance[$attr] ?? "0.00");
+                },
+            ];
+        }
+
         return array_merge(parent::columns(), [
             'serie' => [
                 'class' => XEditableColumn::class,
@@ -31,6 +74,8 @@ class RequisiteGridView extends ContactGridView
                 'class' => MenuColumn::class,
                 'menuClass' => RequisiteActionsMenu::class,
             ],
-        ]);
+            ], $curColumns ?? [],
+            $balanceColumns ?? []
+        );
     }
 }
--- src/views/requisite/_search.php
+++ src/views/requisite/_search.php
@@ -2,10 +2,19 @@
 
 use hipanel\modules\client\widgets\combo\ClientCombo;
 use hipanel\modules\client\widgets\combo\SellerCombo;
+use hipanel\modules\finance\helpers\CurrencyFilter;
+use hipanel\widgets\DateTimePicker;
+use hiqdev\combo\StaticCombo;
+use hipanel\models\Ref;
 
 /**
  * @var \hipanel\widgets\AdvancedSearch
  */
+?>
+
+<?php
+$currencies = Ref::getList('type,currency');
+$currencies = CurrencyFilter::addSymbolAndFilter($currencies);
 ?>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
@@ -24,4 +33,29 @@ use hipanel\modules\client\widgets\combo\SellerCombo;
     <div class="col-md-4 col-sm-6 col-xs-12">
         <?= $search->field('seller_id')->widget(SellerCombo::class) ?>
     </div>
+
+    <?php if (in_array($uiModel->representation, ['balance', 'balances'], true)) : ?>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <?= DateTimePicker::widget([
+                'id' => 'balance_time-picker',
+                'model' => $search->model,
+                'attribute' => 'balance_time',
+                'clientOptions' => [
+                    'autoclose' => true,
+                    'minView' => 2,
+                    'format' => 'yyyy-mm-dd',
+                ],
+            ]) ?>
+        </div>
+    <?php endif ?>
+
+    <?php if ($uiModel->representation === 'balance') : ?>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <?= $search->field('currency')->widget(StaticCombo::class, [
+                'data' => $currencies,
+                'hasId' => true,
+                'multiple' => false,
+            ]) ?>
+        </div>
+    <?php endif ?>
 <?php endif ?>
