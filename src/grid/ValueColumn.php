c18115e2099e42d3e72b754d08a992601890a2ed
strorch <smy980807@ukr.net>
2021-04-16 16:46:29 +0300

    minor

--- src/grid/ValueColumn.php
+++ src/grid/ValueColumn.php
@@ -35,7 +35,6 @@ class ValueColumn extends Column
 
     protected function renderDataCellContent($model, $key, $index)
     {
-        /** todo: refactor */
         $fields = Html::hiddenInput("[{$model->id}]object_id", $model->object_id);
         $fields .= Html::hiddenInput("[{$model->id}]type", $model->type);
 
