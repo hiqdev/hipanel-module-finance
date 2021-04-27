c855b5ce68a4bbb1734bc525d43e1e8e2cfaed01
Andrey Klochok <andreyklochok@gmail.com>
2021-04-27 11:44:15 +0300

    Show formula suggestions in a correct currency

--- src/providers/FormulaExamplesProvider.php
+++ src/providers/FormulaExamplesProvider.php
@@ -19,6 +19,13 @@ use Yii;
  */
 class FormulaExamplesProvider
 {
+    private string $currencyCode;
+
+    public function __construct(string $currencyCode)
+    {
+        $this->currencyCode = $currencyCode;
+    }
+
     public function getGroups()
     {
         $result[] = $this->groupOf(Yii::t('hipanel.finance.price', 'Fixed discount'), $this->fixedDiscountFormulas());
@@ -33,7 +40,7 @@ class FormulaExamplesProvider
     {
         return [
             sprintf("discount.fixed('10%%').since('%s').reason('because')", date('m.Y')),
-            sprintf("discount.fixed('11 USD').since('%s').reason('agreed')", date('m.Y')),
+            sprintf("discount.fixed('11 %s').since('%s').reason('agreed')", $this->currencyCode, date('m.Y')),
             sprintf("discount.fixed('20%%').till('%s').reason('loyalty')", date('m.Y', strtotime('+6 months'))),
             sprintf("discount.fixed('5 tb').since('%s').reason('bonus')", date('m.Y')) => Yii::t('hipanel.finance.price', 'Applicable for overuse prices. The example will compensate up to 5 TB of overuse as discount. Make sure to use appropriate unit, such as <code>items</code>, <code>gbps</code> or <code>hours</code>.'),
             sprintf("discount.fixed('5 tb').as('deposit,compensation').since('%s').reason('bonus')", date('m.Y')) => Yii::t('hipanel.finance.price', 'Applicable for overuse prices. Will compensate up to 5 TB of overuse as a separate Compensation bill. You can use any other bill type in option <code>.as()</code>, such as <code>deposit,creditnote</code> or <code>correction,positive</code>.'),
@@ -45,7 +52,7 @@ class FormulaExamplesProvider
         return [
             sprintf("discount.since('%s').grows('10pp').every('month').reason('because')", date('m.Y')) => Yii::t('hipanel.finance.price', '<code>10pp</code> means "10 percent points". A percent point is the arithmetic difference of two percentages. For example, moving up from 40% to 50% is a 10 percentage point increase, but is an actual 25% percent increase in what is being measured.'),
             sprintf("discount.since('%s').grows('10%%').every('month').max('100%%').reason('because')", date('m.Y')),
-            sprintf("discount.since('%s').grows('20 USD').every('2 months').min('30 USD').max('80 USD')", date('m.Y')),
+            sprintf("discount.since('%s').grows('20 %s').every('2 months').min('30 %s').max('80 %s')", date('m.Y'), $this->currencyCode, $this->currencyCode, $this->currencyCode),
             sprintf("discount.since('%s').grows('1%%').every('1 months').min('5%%').max('25%%')", date('m.Y')),
         ];
     }
