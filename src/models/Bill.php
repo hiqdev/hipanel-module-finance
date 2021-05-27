bc2d3b80dccb947d5976ab7f4055935bdb5b2e30
strorch <smy980807@ukr.net>
2021-05-27 17:03:54 +0300

    added balance fields to bill summary table

--- src/models/Bill.php
+++ src/models/Bill.php
@@ -57,7 +57,7 @@ class Bill extends \hipanel\base\Model
             [['object_id', 'tariff_id'], 'integer'],
             [['client', 'seller', 'bill', 'unit', 'requisite'], 'safe'],
             [['domain', 'server'], 'safe'],
-            [['sum', 'balance', 'quantity', 'positive', 'negative'], 'number'],
+            [['sum', 'balance', 'quantity', 'positive', 'negative', 'opening_balance', 'closed_balance'], 'number'],
             [['currency', 'label', 'descr'], 'safe'],
             [['object', 'domains', 'tariff', 'tariff_type'], 'safe'],
             [['type', 'gtype', 'class', 'ftype'], 'safe'],
---
+++ src/widgets/views/BillSummaryTable.php
@@ -0,0 +1,48 @@
+<?php
+
+use hipanel\modules\finance\models\Bill;
+use hipanel\modules\finance\widgets\ColoredBalance;
+
+/**
+ * @var string $tableName
+ * @var array $currencies
+ * @var array $rows
+ * @var array $values
+ */
+
+?>
+
+<div class="summary">
+    <div style="display: flex; flex-wrap: wrap; margin: -1rem">
+        <div class="table-responsive" style="margin: 1rem">
+            <table class="table table-striped table-bordered table-condensed">
+                <thead>
+                <tr>
+                    <td class="text-muted"><?= $tableName ?></td>
+                    <?php foreach ($currencies as $sign): ?>
+                        <th class="text-right"><?= $sign ?></th>
+                    <?php endforeach; ?>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($rows as $type => $label): ?>
+                    <tr>
+                        <td class="text-right"><?= $label ?></td>
+                        <?php foreach ($currencies as $currency => $sign): ?>
+                            <td class="text-right">
+                                <?= ColoredBalance::widget([
+                                    'model' => new Bill([
+                                        'sum' => $values[$type][$currency] ?? '',
+                                        'currency' => $currency,
+                                    ]),
+                                    'attribute' => 'sum',
+                                ]) ?>
+                            </td>
+                        <?php endforeach; ?>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
