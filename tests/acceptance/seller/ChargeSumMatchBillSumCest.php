a098cfdd10c60a1756c0e44b9f91b0f4ef4499e1
Drakorgaur <re.markofdark@gmail.com>
2021-05-31 12:04:34 +0300

    Added ChargeSumMatchBillSum

---
+++ tests/acceptance/seller/ChargeSumMatchBillSumCest.php
@@ -0,0 +1,54 @@
+<?php 
+
+namespace hipanel\modules\finance\tests\acceptance\seller;
+
+use hipanel\modules\finance\tests\_support\Page\bill\Create;
+use hipanel\tests\_support\Step\Acceptance\Seller;
+use Codeception\Example;
+use hipanel\helpers\Url;
+
+class BillChargeSumCest 
+{
+    /**
+     * @dataProvider getBillData
+     */
+    public function ensureChargeSumMatchBillSum(Seller $I, Example $example)
+    {
+        $I->login();
+        $page = new Create($I);
+        $I->needPage(Url::to('@bill/create'));
+        $bill = $this->getBillData();
+        $page->fillMainBillFields(iterator_to_array($example->getIterator()));
+        $page->addCharge($example['charge1']);
+        $page->addCharge($example['charge2']);
+        $I->pressButton('Save');
+        $I->cantSee('Bill sum must match charges sum: -762.7');
+    }
+
+    private function getBillData(): array
+    {
+        return [
+            'client' => [
+                'login'     => 'hipanel_test_user',
+                'type'      => 'Monthly fee',
+                'currency'  => '$',
+                'sum'       =>  762.7,
+                'quantity'  =>  1,
+                'charge1'    => [
+                    'class'     => 'Client',
+                    'objectId'  => 'hipanel_test_user1',
+                    'type'      => 'Monthly fee',
+                    'sum'       => -712.80,
+                    'quantity'  => 1,
+                ],
+                'charge2'      =>[
+                    'class'     => 'Client',
+                    'objectId'  => 'hipanel_test_user2',
+                    'type'      => 'VAT',
+                    'sum'       => -49.90,
+                    'quantity'  => 1,
+                ],
+            ],
+        ];
+    }
+}
