7ebe3099de96631eb4b122e565a63ad4e78e2fe5
Maksym Storchak <smy980807@ukr.net>
2021-06-05 21:41:50 +0300

    added balance fields to bill summary table (#244)
    * added balance fields to bill summary table
    
    * added translations
    
    * minor
    
    Co-authored-by: Andrii Vasyliev <sol@hiqdev.com>
    
    * minor
    
    * rename `closing_balance` <- closed_balance
    
    Co-authored-by: Andrii Vasyliev <sol@hiqdev.com>

--- src/widgets/BillSummaryTable.php
+++ src/widgets/BillSummaryTable.php
@@ -18,71 +18,68 @@ class BillSummaryTable extends Widget
 
     public array $currencies = [];
 
+    private array $rows = [];
+
+    private string $tableName;
+
+    private array $displayBills = [];
+
     public function init(): void
     {
-        $this->currencies = CurrencyFilter::addSymbolAndFilter($this->currencies);
+        parent::init();
+
+        $this->tableName = empty($this->onPageBills) ? 'Summary' : 'Page summary';
+        $this->rows = [
+            'positive' => Yii::t('hipanel:finance', 'Positive'),
+            'negative' => Yii::t('hipanel:finance', 'Negative'),
+            'openingBalance' => Yii::t('hipanel:finance', 'Opening balance'),
+            'closingBalance' => Yii::t('hipanel:finance', 'Closing balance'),
+            'total' => Yii::t('hipanel:finance', 'Total'),
+        ];
+        $this->displayBills = empty($this->onPageBills) ? $this->allBills : $this->onPageBills;
     }
 
     public function run(): string
     {
-        $tables = '';
-        if (!empty($this->onPageBills)) {
-            $tables .= Html::tag('div', $this->buildTable(Yii::t('hipanel:finance', 'Page summary'), $this->onPageBills), ['class' => 'table-responsive', 'style' => 'margin: 1rem']);
-        }
-        if (!empty($this->allBills)) {
-            $tables .= Html::tag('div', $this->buildTable(Yii::t('hipanel:finance', 'Summary'), $this->allBills), ['class' => 'table-responsive', 'style' => 'margin: 1rem']);
+        if (empty($this->displayBills)) {
+            return Html::tag('div', '', ['class' => 'summary']);
         }
 
-        return Html::tag('div', Html::tag('div', $tables, ['style' => 'display: flex; flex-wrap: wrap; margin: -1rem']), ['class' => 'summary']);
+        $values =  $this->calculate();
+        return $this->render('BillSummaryTable', [
+            'currencies' => $this->filterCurrencies($values),
+            'tableName' => $this->tableName,
+            'rows' => $this->rows,
+            'values' => $values,
+        ]);
     }
 
-    private function buildTable(string $name, array $bills): string
+    private function filterCurrencies(array $tableValues): array
     {
-        $currencies = $this->currencies;
-        $rows = [
-            'positive' => Yii::t('hipanel:finance', 'Positive'),
-            'negative' => Yii::t('hipanel:finance', 'Negative'),
-            'total' => Yii::t('hipanel:finance', 'Total'),
-        ];
-        [$positive, $negative, $total] = $this->calculate($bills);
-        $head = Html::beginTag('thead');
-        $head .= Html::beginTag('tr');
-        $head .= Html::tag('td', $name, ['class' => 'text-muted']);
-        foreach ($currencies as $currency => $sign) {
-            if (isset($positive[$currency]) || isset($negative[$currency])) {
-                $head .= Html::tag('th', $sign, ['class' => 'text-right']);
-            } else {
-                unset($currencies[$currency]);
-            }
-        }
-        $head .= Html::endTag('tr');
-        $head .= Html::endTag('thead');
-        $body = Html::beginTag('tbody');
-        foreach ($rows as $type => $label) {
-            $body .= Html::beginTag('tr');
-            $body .= Html::tag('th', $label, ['class' => 'text-right']);
-            foreach ($currencies as $currency => $sign) {
-                $sum = ColoredBalance::widget([
-                    'model' => new Bill(['sum' => $$type[$currency], 'currency' => $currency]),
-                    'attribute' => 'sum',
-                ]);
-                $body .= Html::tag('td', $sum, ['class' => 'text-right']);
-            }
-            $head .= Html::endTag('tr');
-        }
-        $body .= Html::endTag('tbody');
-
-        return Html::tag('table', $head . $body, ['class' => 'table table-striped table-bordered table-condensed']);
+        return array_filter(
+            CurrencyFilter::addSymbolAndFilter($this->currencies),
+            function ($curr) use ($tableValues) {
+                foreach ($tableValues as $rowValues) {
+                     if (!empty($rowValues[$curr])) {
+                         return true;
+                     }
+                 }
+                 return false;
+            },
+            ARRAY_FILTER_USE_KEY,
+        );
     }
 
-    private function calculate(array $bills): array
+    private function calculate(): array
     {
-        $positive = $negative = $total = [];
-        foreach ($bills as $bill) {
+        $positive = $negative = $total = $openingBalance = $closingBalance = [];
+        foreach ($this->displayBills as $bill) {
             if ($this->isGrouped($bill)) {
                 $positive[$bill->currency] = $bill->positive;
                 $negative[$bill->currency] = $bill->negative;
                 $total[$bill->currency] = $bill->sum;
+                $openingBalance[$bill->currency] = $bill->opening_balance;
+                $closingBalance[$bill->currency] = $bill->closing_balance;
             } else if ($bill->sum >= 0) {
                 $positive[$bill->currency] += abs($bill->sum);
                 $total[$bill->currency] += abs($bill->sum);
@@ -92,11 +89,17 @@ class BillSummaryTable extends Widget
             }
         }
 
-        return [$positive, $negative, $total];
+        return [
+            'positive' => $positive,
+            'negative' => $negative,
+            'total' => $total,
+            'openingBalance' => $openingBalance,
+            'closingBalance' => $closingBalance,
+        ];
     }
 
     private function isGrouped(Bill $bill): bool
     {
-        return $bill->negative || $bill->positive;
+        return $bill->negative || $bill->positive || $bill->opening_balance || $bill->closing_balance;
     }
 }
