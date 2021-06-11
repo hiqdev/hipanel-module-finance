5b01fc25142e5fc915b31f8f58f3be3eed70df1c
Drakorgaur <re.markofdark@gmail.com>
2021-06-11 10:08:59 +0300

    Added recharging Account

--- tests/acceptance/seller/InternalTransferCest.php
+++ tests/acceptance/seller/InternalTransferCest.php
@@ -7,15 +7,21 @@ use Codeception\Example;
 use hipanel\tests\_support\Step\Acceptance\Seller;
 use hipanel\tests\_support\Page\Widget\Input\Input;
 use hipanel\tests\_support\Page\Widget\Input\Select2;
-use hipanel\modules\finance\tests\_support\Page\transfer\Create;
+use hipanel\modules\finance\tests\_support\Page\transfer\Create as transferCreate;
+use hipanel\modules\finance\tests\_support\Page\bill\Create as billCreate;
 use hipanel\modules\finance\tests\_support\Page\transfer\Index;
 
 class InternalTransferCest
 {
-     /**
-     * @var Create
+    /**
+     * @var transferCreate
      */
-    private $create;
+    private $transferCreate;
+
+    /**
+     * @var billCreate
+     */
+    private $billCreate;
 
     /**
      * @var Index
@@ -24,8 +30,21 @@ class InternalTransferCest
 
     public function _before(Seller $I)
     {
-        $this->create = new Create($I);
+        $this->transferCreate = new transferCreate($I);
+        $this->billCreate = new billCreate($I);
         $this->index = new Index($I);
+    }
+
+    /**
+     * @dataProvider provideTransferData
+     */
+    public function rechargeAccount(Seller $I, Example $example): void
+    {
+        $I->needPage(Url::to('@bill/create'));
+        $this->billCreate->fillMainBillFields($example['bill']);
+        $I->pressButton('Save');
+        $this->billCreate->seeActionSuccess();
+        $I->wait(3);
     }
 
     /**
@@ -35,6 +54,7 @@ class InternalTransferCest
     {
         $I->login();
         $exampleArray = iterator_to_array($example->getIterator());
+
         $I->needPage(Url::to('@bill/create-transfer'));
         $I->see('Add internal transfer', 'h1');
         $this->ensureICantCreateTransferWithoutRequiredData($I);
@@ -46,12 +66,12 @@ class InternalTransferCest
     private function ensureICantCreateTransferWithoutRequiredData(Seller $I): void
     {
         $I->click('Save');
-        $this->create->containsBlankFieldsError(['Sum' ,'Client', 'Receiver ID', 'Currency']);
+        $this->transferCreate->containsBlankFieldsError(['Sum' ,'Client', 'Receiver ID', 'Currency']);
     }
 
     private function ensureICanCreateInternalTransfer(Seller $I, $transferData): void
     {
-        $this->create->fillMainInternalTransferFields($transferData['transfer']);
+        $this->transferCreate->fillMainInternalTransferFields($transferData['transfer']);
     }
 
     private function provideTransferData(): array
@@ -59,10 +79,17 @@ class InternalTransferCest
         return [
             'transfers' => [
                 'transfer' => [
-                    'sum'        => 1,
+                    'sum'        => 1000,
                     'client'     => 'hipanel_test_user',
                     'receiverId' => 'hipanel_test_user2',
                 ],
+                'bill' => [
+                    'login'     => 'hipanel_test_user',
+                    'type'      => 'Monthly fee',
+                    'currency'  => '$',
+                    'sum'       =>  1200,
+                    'quantity'  =>  1,
+                ]
             ],
         ];
     }
