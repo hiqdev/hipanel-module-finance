00a38440971d73ec8d484e98400d59c0267d2cfb
Mark <83531949+Drakorgaur@users.noreply.github.com>
2021-06-15 11:43:19 +0300

    Added BillCopyCest and Modified Create (#246)
    * Added BillCopyCest and Modified Create
    
    * Removed before
    
    * Added Example
    
    * Parted method
    
    * Added method grabFromCurrentUrl
    
    * Edited BillCopyCest and Create
    
    * Edited BillCopyCest and Create, Update
    
    * Edited Bill, Added new file - Copy.php, Updated Create and Index
    
    * Edited BillCopyCest & Create
    
    * TypeHint Edited
    
    * Edited Create and BillCopyCest typehint
    
    * Edited Create and BillCopyCest
    
    * Updated Create
    
    * Edited BillCopyCest and Create
    
    * Edited Create
    
    * Edited Copy.php
    
    * Added PressButtonHelper
    
    * Resolved Conflict
    
    * TypeHint

---
+++ tests/_support/Page/bill/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace hipanel\modules\finance\tests\_support\Page\bill;
+
+use hipanel\helpers\Url;
+use hipanel\tests\_support\Page\Authenticated;
+
+class Index extends hipanel\tests\_support\Page\IndexPage
+{   
+    public function seeUpdateSuccess(): ?string
+    {
+        $I = $this->tester;
+
+        $I->closeNotification('Bill was updated successfully');
+        $I->seeInCurrentUrl('/finance/bill?id');
+
+        return $this->grabBillIdFromUrl();
+    }
+}
