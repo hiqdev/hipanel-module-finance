813c114e729df16d6dbba731b57e7280c32121e9
Drakorgaur <re.markofdark@gmail.com>
2021-06-15 14:57:03 +0300

    View Update

--- tests/_support/Page/bill/View.php
+++ tests/_support/Page/bill/View.php
@@ -10,29 +10,22 @@
 
 namespace hipanel\modules\finance\tests\_support\Page\bill;
 
-use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Page\Authenticated;
+use hipanel\tests\_support\Page\Widget\Grid;
 use hipanel\helpers\Url;
 
-class View extends Create
+class View extends Authenticated
 {
     public function viewBillById(string $billId): void
     {
         $this->tester->needPage(Url::to("@bill/view?id=$billId"));
     }
 
-    public function ensureBillViewContainsData(array $billData): void
-    {
-        $I = $this->tester;
-
-        $indexPage = new IndexPage($I);
-        $indexPage->gridView->BillViewContainsData($billData);
-    }
-
     public function ensureChargeViewContainsData(array $chargeData): void
     {
         $I = $this->tester;
 
-        $indexPage = new IndexPage($I);
-        $indexPage->gridView->ChargeViewContainsData($billData);
+        $gridView = new Grid($I);
+        $gridView->containsDataInTable($chargeData);
     }
 }
