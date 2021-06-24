65d25f41958b4d1d4f331b16828be05f4c6e7dd2
Drakorgaur <re.markofdark@gmail.com>
2021-06-24 12:25:40 +0300

    Aded Check if user have negative balance

--- tests/acceptance/seller/InternalTransferCest.php
+++ tests/acceptance/seller/InternalTransferCest.php
@@ -10,29 +10,42 @@ use hipanel\tests\_support\Page\Widget\Input\Select2;
 use hipanel\modules\finance\tests\_support\Page\transfer\Create as TransferCreate;
 use hipanel\modules\finance\tests\_support\Page\bill\Create as BillCreate;
 use hipanel\modules\finance\tests\_support\Page\transfer\Index;
+use hipanel\tests\_support\Page\IndexPage;
 
 class InternalTransferCest
 {
     private TransferCreate $transferCreate;
     private BillCreate $billCreate;
     private Index $index;
+    private IndexPage $indexPage;
 
     public function _before(Seller $I): void
     {
         $this->transferCreate = new TransferCreate($I);
         $this->billCreate = new BillCreate($I);
         $this->index = new Index($I);
+        $this->indexPage = new IndexPage($I);
     }
 
     /**
-     * IMPORTANT: this bill is needed because transfer can not be created when client has low balance
-     * TODO: remove bill creation when transfer rules will be changed
+    * IMPORTANT: this bill is needed because transfer can not be created when client has low balance
+    * TODO: remove bill creation when transfer rules will be changed
+    * @dataProvider provideTransferData
+    */
+
+    /**
      * @dataProvider provideTransferData
      */
     public function rechargeAccount(Seller $I, Example $example): void
     {
+        $billData = iterator_to_array($example->getIterator()); 
+        $billData = $this->checkIfUserHaveNegativeBalance($I, $billData);
+
         $I->needPage(Url::to('@bill/create'));
         $this->billCreate->fillMainBillFields($example['bill']);
+        $this->billCreate->addCharges($billData['bill']['charges']);
+        $this->billCreate->getAndSetBillSumFromCharges();
+        $this->billCreate->clickToggleSign();
         $I->pressButton('Save');
         $this->billCreate->seeActionSuccess();
     }
@@ -53,6 +66,26 @@ class InternalTransferCest
         $this->index->seeTransferActionSuccess();
     }
 
+    private function checkIfUserHaveNegativeBalance(Seller $I, array $billInfo)
+    {
+        $I->needPage(Url::to('@finance/bill'));
+
+        $this->indexPage->filterBy(Select2::asTableFilter($I, 'Client'), $billInfo['transfer']['client']);
+        $rowNumber = $this->indexPage->gridView->getRowNumberByNameFromSummary('Total');
+        $sum = $I->grabTextFrom("//div[@class='summary']//tbody//tr[$rowNumber]//td//span");
+
+        $sum = $this->transofrmSum($sum);
+        if ($sum) {
+            $sum = (int)$sum;
+            $billInfo['bill']['charges']['charge2'] = $billInfo['bill']['charges']['charge1'];
+            
+            $billInfo['bill']['charges']['charge2']['type'] = 'PayPal';
+            $billInfo['bill']['charges']['charge2']['sum'] = ++$sum;
+        }
+
+        return $billInfo;
+    }
+
     private function ensureICantCreateTransferWithoutRequiredData(Seller $I): void
     {
         $I->click('Save');
@@ -64,22 +97,45 @@ class InternalTransferCest
         $this->transferCreate->fillMainInternalTransferFields($transferData['transfer']);
     }
 
+    private function transofrmSum(string $currentBalance): ?string
+    {
+
+        $repl = [',' => ''];
+        if(similar_text($currentBalance, '-')) {
+            $currentBalance = substr_replace($currentBalance, '', 0, 2);
+            $currentBalance = strtr($currentBalance, $repl);
+        } else {
+            return null;
+        }
+
+        return $currentBalance;
+    }
+
     private function provideTransferData(): array
     {
         return [
             'transfers' => [
                 'transfer' => [
                     'sum'        => 1000,
-                    'client'     => 'hipanel_test_user',
+                    'client'     => 'hipanel_test_user1',
                     'receiverId' => 'hipanel_test_user2',
                 ],
                 'bill' => [
-                    'login'     => 'hipanel_test_user',
+                    'login'     => 'hipanel_test_user1',
                     'type'      => 'Monthly fee',
                     'currency'  => '$',
-                    'sum'       =>  1200,
+                    'sum'       =>  0,
                     'quantity'  =>  1,
-                ]
+                    'charges'   => [
+                        'charge1'   => [
+                            'class' => 'Client',
+                            'objectId' => 'hipanel_test_user1',
+                            'type' => 'Monthly fee',
+                            'sum' => 1000,
+                            'quantity' => '1',
+                        ],
+                    ],
+                ],
             ],
         ];
     }
