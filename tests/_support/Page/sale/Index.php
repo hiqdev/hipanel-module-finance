d5880cdc0e534207adb009a6c4bc6657bbcd093d
mark <re.markofdark@gmail.com>
2021-09-06 11:06:37 +0300

    Modified test

---
+++ tests/_support/Page/sale/Index.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+
+namespace hipanel\modules\finance\tests\_support\Page\sale;
+
+use hipanel\tests\_support\Page\Authenticated;
+use hipanel\tests\_support\Page\Widget\Input\Select2;
+use hipanel\tests\_support\Helper\PressButtonHelper;
+
+class Index extends Authenticated
+{
+    public function deleteSelectedSales(): void
+    {
+        $I = $this->tester;
+        $I->pressButton('Delete');
+        $I->acceptPopup();
+        $this->seeDeleteActionSuccess();
+    }
+
+    private function seeDeleteActionSuccess(): void
+    {
+        $this->tester->closeNotification('Sale was successfully deleted.');
+    }
+}
--- tests/acceptance/seller/SalesCest.php
+++ tests/acceptance/seller/SalesCest.php
@@ -7,21 +7,66 @@ use hipanel\helpers\Url;
 use Codeception\Example;
 use hipanel\tests\_support\Page\IndexPage;
 use hipanel\modules\finance\tests\_support\Page\sale\Edit;
+use hipanel\modules\finance\tests\_support\Page\sale\Sale;
+use hipanel\modules\finance\tests\_support\Page\sale\Index;
 use hipanel\tests\_support\Page\Widget\Input\Select2;
 use hipanel\tests\_support\Step\Acceptance\Seller;
+use hipanel\tests\_support\Step\Acceptance\Manager;
 
 class SalesIndexPageCest
 {
     /**
-     * @dataProvider getSaleData
+     * @dataProvider getSaleDataForManager
+     */
+    public function EnsureICanCreateSeveralSales(Manager $I, Example $example): void
+    {
+        $index = new IndexPage($I);
+        $sale = new Sale($I);
+        $saleData = iterator_to_array($example->getIterator());
+
+        $I->login();
+        $I->needPage('/server/server');
+        $I->waitForPageUpdate();
+
+        $column = $index->gridView->getColumnNumber('DC');
+        $row[] = $index->getRowNumberInColumnByValue($column, 'TEST-DS-01');
+        $row[] = $index->getRowNumberInColumnByValue($column, 'TEST-DS-02');
+
+        foreach ($row as $currentRow) {
+            $I->needPage('/server/server');
+            $index->openRowMenuByNumber($currentRow);
+            $index->chooseRowMenuOption('View');
+            $I->clickLink('Change tariff');
+            $I->waitForElementVisible("//div[contains(text(),'Affected items')]", 90);
+
+            $sale->fillSaleFields($saleData);
+            
+            $I->pressButton('Sell');
+            $I->waitForPageUpdate();
+            $I->closeNotification('Servers were sold');
+        }
+
+        $I->logout($I);
+    }
+
+    /**
+     * @dataProvider getSaleDataForSeller
      */
     public function EnsureICanEditSeveralSales(Seller $I, Example $example): void
     {
-        $I->login();
+        $index = new IndexPage($I);
         $edit = new Edit($I);
+
+        $I->login();
         $saleData = iterator_to_array($example->getIterator());
 
         $I->needPage(Url::to('@sale/index'));
+
+        $index->setAdvancedFilter(Select2::asAdvancedSearch($I, 'Tariff'), $saleData['tariff']);
+        $I->pressButton('Search');
+
+        $I->waitForPageUpdate();
+
         $I->checkOption("//tbody/tr[1]//input[1]");
         $I->checkOption("//tbody/tr[2]//input[1]");
         $I->pressButton('Edit');
@@ -53,11 +98,43 @@ class SalesIndexPageCest
         $edit->checkDetailViewData($saleData);
     }
 
-    protected function getSaleData(): array
+    /**
+     * @dataProvider getSaleDataForSeller
+     */
+    public function EnsureICanDeleteSeveralSales(Seller $I, Example $example): void
+    {
+        $index = new Index($I);
+        $indexPage = new IndexPage($I);
+        $saleData = iterator_to_array($example->getIterator());
+
+        $I->needPage(Url::to('@sale/index'));
+
+        $indexPage->setAdvancedFilter(Select2::asAdvancedSearch($I, 'Tariff'), $saleData['tariff']);
+        $I->pressButton('Search');
+
+        $I->waitForPageUpdate();
+
+        $I->checkOption("//tbody/tr[1]//input[1]");
+        $I->checkOption("//tbody/tr[2]//input[1]");
+
+        $index->deleteSelectedSales();
+    }
+
+    protected function getSaleDataForSeller(): array
     {
         return [
             'sale' => [
-                'tariff' => 'Sol Test 1@dsr'
+                'tariff' => 'PlanForkerViaLegacyApiTest / Plan to be clonned@hipanel_test_reseller',
+            ],
+        ];
+    }
+
+    protected function getSaleDataForManager(): array
+    {
+        return [
+            'sale' => [
+                'client' => 'testuser60798ee837548@test1.test1',
+                'tariff' => 'PlanForkerViaLegacyApiTest / Plan to be clonned@hipanel_test_reseller',
             ],
         ];
     }
