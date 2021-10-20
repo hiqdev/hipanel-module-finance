62593437f4642d08daea1dd22c6efa2da3225f0c
Andrey Klochok <andreyklochok@gmail.com>
2021-10-20 09:40:36 +0300

    Add test Sale filter by Object and Tariff on the index page (#307)

--- tests/acceptance/client/SalesCest.php
+++ tests/acceptance/client/SalesCest.php
@@ -11,11 +11,22 @@
 namespace hipanel\modules\finance\tests\acceptance\client;
 
 use hipanel\helpers\Url;
+use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Page\Widget\Input\Input;
 use hipanel\tests\_support\Step\Acceptance\Client;
 
 class SalesCest
 {
-    public function ensureIndexPageWorks(Client $I)
+    private IndexPage $indexPage;
+
+    public function _before(Client $I): void
+    {
+        $I->login();
+        $I->needPage(Url::to('@sale/index'));
+        $this->indexPage = new IndexPage($I);
+    }
+
+    public function ensureIndexPageWorks(Client $I): void
     {
         $I->login();
         $I->needPage(Url::to('@sale/index'));
@@ -23,15 +34,27 @@ class SalesCest
         $this->ensureICanSeeAdvancedSearchBox($I);
         $this->ensureICanSeeBodyBox($I);
     }
-    private function ensureICanSeeAdvancedSearchBox(Client $I)
+
+    public function ensureFilterByObjectNameWorks(Client $I): void
     {
-        $url = Url::to('@sale/index');
-        $form = "//form[@action='$url']";
+        $this->indexPage->filterBy(Input::asTableFilter($I, 'Object'), 'DSTEST01');
+        $this->indexPage->containsAmountOfRows(1);
+    }
+
+    public function ensureFilterByTariffNameWorks(Client $I): void
+    {
+        $this->indexPage->filterBy(Input::asTableFilter($I, 'Tariff'), 'test dsr@dsr');
+        $this->indexPage->containsAmountOfRows(1);
+    }
+
+    private function ensureICanSeeAdvancedSearchBox(Client $I): void
+    {
         $I->see('Advanced search', 'h3');
         $I->seeElement('input', ['id' => 'salesearch-object_inilike']);
         $I->see('Search', 'button');
     }
-    private function ensureICanSeeBodyBox(Client $I)
+
+    private function ensureICanSeeBodyBox(Client $I): void
     {
         $I->seeLink('Type');
         $I->seeElement('input', ['name' => 'SaleSearch[object_type]']);
