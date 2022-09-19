e1fe5c886d9ecd4eccdbde7d1479dc968d9afa18
gadezist <39760138+gadezist@users.noreply.github.com>
2022-09-19 10:39:14 +0300

    HP-1010 Add export button to Finance >> Charges (#372)
    * HP-1010 Add export button to Finance >> Charges
    
    * HP-1010 add bill_id to export

---
+++ src/grid/ChargeRepresentations.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Finance module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-finance
+ * @package   hipanel-module-finance
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2019, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\finance\grid;
+
+use hiqdev\higrid\representations\RepresentationCollection;
+use Yii;
+
+class ChargeRepresentations extends RepresentationCollection
+{
+    protected function fillRepresentations()
+    {
+        $this->representations = array_filter([
+            'common' => [
+                'label' => Yii::t('hipanel', 'common'),
+                'columns' => [
+                    'checkbox',
+                    'bill_id',
+                    'client_id',
+                    'seller',
+                    'tariff',
+                    'name',
+                    'type',
+                    'currency',
+                    'sum',
+                    'quantity',
+                    'is_payed',
+                    'time',
+                    'description',
+                ],
+            ],
+        ]);
+    }
+}
