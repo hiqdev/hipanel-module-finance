3c49ba6b7f7e81f71da79ae571bc5479a45a33d7
gadezist <andrybitko@gmail.com>
2022-09-28 15:37:06 +0300

    HP-1037 Remove PJAX form HiPanel

--- src/views/plan/index.php
+++ src/views/plan/index.php
@@ -1,56 +1,63 @@
 <?php
 
 use hipanel\modules\finance\grid\PlanGridView;
+use hipanel\modules\finance\models\PlanSearch;
 use hipanel\widgets\IndexPage;
-use hipanel\widgets\Pjax;
+use hiqdev\hiart\ActiveDataProvider;
 use yii\helpers\Html;
+use yii\web\View;
+
+/**
+ * @var View $this
+ * @var array $states
+ * @var PlanSearch $model
+ * @var ActiveDataProvider $dataProvider
+ */
 
 $this->title = Yii::t('hipanel:finance', 'Tariff plans');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
-    <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
+<?php $page = IndexPage::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
 
-        <?php $page->setSearchFormData([]) ?>
+    <?php $page->setSearchFormData([]) ?>
 
-        <?php $page->beginContent('main-actions') ?>
-            <?php if (Yii::$app->user->can('plan.create')) : ?>
-                <?= Html::a(Yii::t('hipanel', 'Create'), ['/finance/plan/create'], ['class' => 'btn btn-sm btn-success']) ?>
-            <?php endif ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('main-actions') ?>
+        <?php if (Yii::$app->user->can('plan.create')) : ?>
+            <?= Html::a(Yii::t('hipanel', 'Create'), ['/finance/plan/create'], ['class' => 'btn btn-sm btn-success']) ?>
+        <?php endif ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('sorter-actions') ?>
-            <?= $page->renderSorter(['attributes' => ['id']]) ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('sorter-actions') ?>
+        <?= $page->renderSorter(['attributes' => ['id']]) ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('bulk-actions') ?>
-            <?php if (Yii::$app->user->can('plan.create')) : ?>
-                <?= $page->renderBulkButton('@plan/restore', Yii::t('hipanel.finance.plan', 'Restore')) ?>
-            <?php endif ?>
-            <?php if (Yii::$app->user->can('plan.update')) : ?>
-                <?= $page->renderBulkDeleteButton('@plan/delete') ?>
-            <?php endif ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('bulk-actions') ?>
+        <?php if (Yii::$app->user->can('plan.create')) : ?>
+            <?= $page->renderBulkButton('@plan/restore', Yii::t('hipanel.finance.plan', 'Restore')) ?>
+        <?php endif ?>
+        <?php if (Yii::$app->user->can('plan.update')) : ?>
+            <?= $page->renderBulkDeleteButton('@plan/delete') ?>
+        <?php endif ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('table') ?>
-            <?php $page->beginBulkForm() ?>
-                <?= PlanGridView::widget([
-                    'boxed' => false,
-                    'dataProvider' => $dataProvider,
-                    'filterModel' => $model,
-                    'columns' => [
-                        'checkbox',
-                        'actions',
-                        'name',
-                        'client',
-                        'type',
-                        'state',
-                        'custom_attributes',
-                    ],
-                ]) ?>
-            <?php $page->endBulkForm() ?>
-        <?php $page->endContent() ?>
-    <?php $page->end() ?>
-<?php Pjax::end() ?>
+    <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+            <?= PlanGridView::widget([
+                'boxed' => false,
+                'dataProvider' => $dataProvider,
+                'filterModel' => $model,
+                'columns' => [
+                    'checkbox',
+                    'actions',
+                    'name',
+                    'client',
+                    'type',
+                    'state',
+                    'custom_attributes',
+                ],
+            ]) ?>
+        <?php $page->endBulkForm() ?>
+    <?php $page->endContent() ?>
+<?php $page->end() ?>
--- src/views/requisite/index.php
+++ src/views/requisite/index.php
@@ -1,11 +1,24 @@
 <?php
 
+use hipanel\models\IndexPageUiOptions;
 use hipanel\modules\finance\grid\RequisiteGridView;
+use hipanel\modules\finance\models\RequisiteSearch;
+use hipanel\modules\server\grid\HubRepresentations;
 use hipanel\widgets\IndexPage;
-use hipanel\widgets\Pjax;
+use hiqdev\hiart\ActiveDataProvider;
 use yii\helpers\Html;
 use hipanel\widgets\AjaxModal;
 use yii\bootstrap\Modal;
+use yii\web\View;
+
+/**
+ * @var View $this
+ * @var array $states
+ * @var RequisiteSearch $model
+ * @var ActiveDataProvider $dataProvider
+ * @var HubRepresentations $representationCollection
+ * @var IndexPageUiOptions $uiModel
+ */
 
 $this->registerCss("
 .balance-cell { display: flex; flex-direction: column; height: 100%; text-align: center; }
@@ -17,61 +30,60 @@ $this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formNam
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
-    <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
 
-        <?php $page->setSearchFormData(compact(['uiModel'])) ?>
+<?php $page = IndexPage::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
 
-        <?php $page->beginContent('main-actions') ?>
-            <?= Html::a(Yii::t('hipanel', 'Create'), '@requisite/create', ['class' => 'btn btn-sm btn-success']) ?>
-        <?php $page->endContent() ?>
+    <?php $page->setSearchFormData(['uiModel' => $uiModel]) ?>
 
-        <?php $page->beginContent('sorter-actions') ?>
-            <?= $page->renderSorter([
-                'attributes' => [
-                    'email',
-                    'name',
-                    'client',
-                    'seller',
-                ],
+    <?php $page->beginContent('main-actions') ?>
+        <?= Html::a(Yii::t('hipanel', 'Create'), '@requisite/create', ['class' => 'btn btn-sm btn-success']) ?>
+    <?php $page->endContent() ?>
+
+    <?php $page->beginContent('sorter-actions') ?>
+        <?= $page->renderSorter([
+            'attributes' => [
+                'email',
+                'name',
+                'client',
+                'seller',
+            ],
+        ]) ?>
+    <?php $page->endContent() ?>
+    <?php $page->beginContent('representation-actions') ?>
+        <?= $page->renderRepresentations($representationCollection) ?>
+    <?php $page->endContent() ?>
+
+    <?php $page->beginContent('bulk-actions') ?>
+        <?= AjaxModal::widget([
+                'id' => 'bulk-set-serie-modal',
+                'bulkPage' => true,
+                'header' => Html::tag('h4', Yii::t('hipanel:finance', 'Set serie'), ['class' => 'modal-title']),
+                'scenario' => 'bulk-set-serie',
+                'actionUrl' => ['@requisite/bulk-set-serie'],
+                'size' => Modal::SIZE_LARGE,
+                'toggleButton' => ['label' => Yii::t('hipanel:finance', 'Set serie'), 'class' => 'btn btn-sm btn-default'],
+        ]) ?>
+
+        <?= AjaxModal::widget([
+                'id' => 'bulk-set-templates-modal',
+                'bulkPage' => true,
+                'header' => Html::tag('h4', Yii::t('hipanel:finance', 'Set templates'), ['class' => 'modal-title']),
+                'scenario' => 'bulk-set-templates',
+                'actionUrl' => ['@requisite/bulk-set-templates'],
+                'size' => Modal::SIZE_LARGE,
+                'toggleButton' => ['label' => Yii::t('hipanel:finance', 'Set templates'), 'class' => 'btn btn-sm btn-default'],
+        ]) ?>
+    <?php $page->endContent() ?>
+
+
+    <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+            <?= RequisiteGridView::widget([
+                'dataProvider' => $dataProvider,
+                'boxed' => false,
+                'filterModel'  => $model,
+                'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
             ]) ?>
-        <?php $page->endContent() ?>
-        <?php $page->beginContent('representation-actions') ?>
-            <?= $page->renderRepresentations($representationCollection) ?>
-        <?php $page->endContent() ?>
-
-        <?php $page->beginContent('bulk-actions') ?>
-            <?= AjaxModal::widget([
-                    'id' => 'bulk-set-serie-modal',
-                    'bulkPage' => true,
-                    'header' => Html::tag('h4', Yii::t('hipanel:finance', 'Set serie'), ['class' => 'modal-title']),
-                    'scenario' => 'bulk-set-serie',
-                    'actionUrl' => ['@requisite/bulk-set-serie'],
-                    'size' => Modal::SIZE_LARGE,
-                    'toggleButton' => ['label' => Yii::t('hipanel:finance', 'Set serie'), 'class' => 'btn btn-sm btn-default'],
-            ]) ?>
-
-            <?= AjaxModal::widget([
-                    'id' => 'bulk-set-templates-modal',
-                    'bulkPage' => true,
-                    'header' => Html::tag('h4', Yii::t('hipanel:finance', 'Set templates'), ['class' => 'modal-title']),
-                    'scenario' => 'bulk-set-templates',
-                    'actionUrl' => ['@requisite/bulk-set-templates'],
-                    'size' => Modal::SIZE_LARGE,
-                    'toggleButton' => ['label' => Yii::t('hipanel:finance', 'Set templates'), 'class' => 'btn btn-sm btn-default'],
-            ]) ?>
-        <?php $page->endContent() ?>
-
-
-        <?php $page->beginContent('table') ?>
-            <?php $page->beginBulkForm() ?>
-                <?= RequisiteGridView::widget([
-                    'dataProvider' => $dataProvider,
-                    'boxed' => false,
-                    'filterModel'  => $model,
-                    'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
-                ]) ?>
-            <?php $page->endBulkForm() ?>
-        <?php $page->endContent() ?>
-    <?php $page->end() ?>
-<?php Pjax::end() ?>
+        <?php $page->endBulkForm() ?>
+    <?php $page->endContent() ?>
+<?php $page->end() ?>
