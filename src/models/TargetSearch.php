b7754d96e3180617eb0d424e60c5d93bac33aa61
Andrey Klochok <andreyklochok@gmail.com>
2023-07-17 17:10:36 +0300

    HP-1467: tags stuff (#450)
    * HP-1467: tags stuff
    
    * HP-1467: tags stuff, minor refactoring
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Dmytro Naumenko <d.naumenko.a@gmail.com>

--- src/models/TargetSearch.php
+++ src/models/TargetSearch.php
@@ -3,10 +3,21 @@
 namespace hipanel\modules\finance\models;
 
 use hipanel\base\SearchModelTrait;
+use yii\helpers\ArrayHelper;
 
 class TargetSearch extends Target
 {
     use SearchModelTrait {
         searchAttributes as defaultSearchAttributes;
+    }
+
+    public static function tableName()
+    {
+        return 'target';
+    }
+
+    public function searchAttributes()
+    {
+        return ArrayHelper::merge($this->defaultSearchAttributes(), ['tags']);
     }
 }
