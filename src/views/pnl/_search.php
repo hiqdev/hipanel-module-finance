ebd7e5ea7258d6c020fadd167f5ae8c567ecf8b0
Andrii Klochok <andreyklochok@gmail.com>
2023-12-10 18:50:01 +0200

    HP-1565

--- src/views/pnl/_search.php
+++ src/views/pnl/_search.php
@@ -1,1 +1,27 @@
 <?php
+
+/** @var AdvancedSearch $search */
+
+use hipanel\widgets\AdvancedSearch;
+
+?>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('charge_id') ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('type') ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('month') ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('has_no_type', ['options' => ['class' => 'form-group checkbox']])->checkbox(['class' => 'option-input']) ?>
+</div>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+    <?= $search->field('has_note', ['options' => ['class' => 'form-group checkbox']])->checkbox(['class' => 'option-input']) ?>
+</div>
