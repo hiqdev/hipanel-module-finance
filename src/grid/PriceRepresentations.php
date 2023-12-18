3b88308b6ad9ac7eecacc774b4736d9022037250
Andrii Klochok <andreyklochok@gmail.com>
2023-12-18 20:28:09 +0200

    HP-1729: add export to Prices page

---
+++ src/grid/PriceRepresentations.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Finance module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-finance
+ * @package   hipanel-module-finance
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2021, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\finance\grid;
+
+use hiqdev\higrid\representations\RepresentationCollection;
+use Yii;
+
+class PriceRepresentations extends RepresentationCollection
+{
+    protected function fillRepresentations(): void
+    {
+        $this->representations = array_filter([
+            'common' => [
+                'label' => Yii::t('hipanel', 'common'),
+                'columns' => [
+                    'checkbox',
+                    'object->name',
+                    'object->label',
+                    'price',
+                    'type',
+                    'note',
+                    'plan',
+                ],
+            ],
+        ]);
+    }
+}
