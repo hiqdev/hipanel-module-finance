b363c3dc25cc287df87802658c59ea8051bfae1f
Andrii Klochok <andreyklochok@gmail.com>
2024-01-11 18:50:22 +0200

    HP-1769

--- src/assets/PnlApp/use/totals.js
+++ src/assets/PnlApp/use/totals.js
@@ -17,25 +17,25 @@ const summirizeRows = (months, rows) => {
   return sum;
 };
 
-const useTotals = (rows, months) => {
-  const revenuesRows = filter(rows, (entry) => entry.type.startsWith("revenues,"));
-  const expensesRows = filter(rows, (entry) => entry.type === "expenses,");
-  const directExpensesRows = filter(rows, (entry) => entry.type.includes(",direct_expenses"));
-  const taxWithoutVatRows = filter(rows, (entry) => entry.type.startsWith("tax,") && !entry.type.includes("tax,vat"));
+const useTotals = (flatRows, months) => {
+  const revenuesRows = filter(flatRows, (entry) => entry.type.startsWith("revenues,"));
+  const expensesRows = filter(flatRows, (entry) => entry.type.startsWith("expenses,"));
+  // const directExpensesRows = filter(rows, (entry) => entry.type.includes(",direct_expenses"));
+  const taxWithoutVatRows = filter(flatRows, (entry) => entry.type.startsWith("tax,") && !entry.type.includes("tax,vat"));
 
   const revenues = summirizeRows(months, revenuesRows);
   const expenses = summirizeRows(months, expensesRows);
-  const directExpenses = summirizeRows(months, directExpensesRows);
+  // const directExpenses = summirizeRows(months, directExpensesRows);
   const taxes = summirizeRows(months, taxWithoutVatRows);
 
   const total_before_taxes = revenues - Math.abs(expenses);
   const profit = total_before_taxes - Math.abs(taxes);
-  const gross_profit_margin = (((revenues - Math.abs(directExpenses)) / revenues) / 100) * 100;
+  // const gross_profit_margin = (((revenues - Math.abs(directExpenses)) / revenues) / 100) * 100;
 
   return {
     total_before_taxes,
     profit,
-    gross_profit_margin,
+    // gross_profit_margin,
   };
 };
 
