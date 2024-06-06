b2148d2081b9469893f0fca8ac53de9c881b6de1
Vadym Hrechukha <vadimgrechuha@gmail.com>
2024-06-06 13:10:57 +0300

    HP-1982: added bootstrap.php file to config of finance module

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,21 +1,6 @@
 <?php
-/**
- * Finance module for HiPanel
- *
- * @link      https://github.com/hiqdev/hipanel-module-finance
- * @package   hipanel-module-finance
- * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2019, HiQDev (http://hiqdev.com/)
- */
+define('APP_TYPE', 'tests');
 
-use Yiisoft\Composer\Config\Builder;
+require_once __DIR__ . '/../config/bootstrap.php';
 
-error_reporting(E_ALL & ~E_NOTICE);
-date_default_timezone_set('UTC');
-$config = require Builder::path('web');
-
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
-\Yii::setAlias('@root', dirname(__DIR__, 4));
-\Yii::$app = new \yii\web\Application($config);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_DEPRECATED);
