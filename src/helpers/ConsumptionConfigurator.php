bd54a868f8005e4e7526f10e77505264234e0da6
Vadym Hrechukha <vadimgrechuha@gmail.com>
2025-01-07 13:31:54 +0200

    HP-1751 optimized Consumption page

--- src/helpers/ConsumptionConfigurator.php
+++ src/helpers/ConsumptionConfigurator.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace hipanel\modules\finance\helpers;
 
@@ -17,7 +15,9 @@ use Yii;
 
 final class ConsumptionConfigurator
 {
-    public array $configurations = [];
+    private ?array $configurations = null;
+
+    private ?array $allPossibleColumnsWithLabels = null;
 
     public function __construct(private readonly BillingRegistryInterface $billingRegistry)
     {
@@ -26,119 +26,6 @@ final class ConsumptionConfigurator
     public function getColumns(string $class): array
     {
         return $this->getConfigurationByClass($class)['columns'];
-    }
-
-    private function getGroups(string $class): array
-    {
-        $groups = [];
-        $columns = $this->getColumns($class);
-        foreach ($this->getConfigurationByClass($class)['groups'] as $group) {
-            $groups[] = $group;
-            foreach ($group as $item) {
-                $columns = array_diff($columns, [$item]);
-            }
-        }
-        foreach ($columns as $column) {
-            $groups[] = [$column];
-        }
-
-        return $groups;
-    }
-
-    public function getGroupsWithLabels(string $class): array
-    {
-        $groups = [];
-        $columnsWithLabels = $this->getColumnsWithLabels($class);
-        foreach ($this->getGroups($class) as $i => $group) {
-            foreach ($group as $j => $type) {
-                $groups[$i][$type] = $columnsWithLabels[$type];
-            }
-        }
-
-        return $groups;
-    }
-
-    public function getColumnsWithLabels(string $class): array
-    {
-        $result = [];
-        foreach ($this->getColumns($class) as $column) {
-            $decorator = $this->getDecorator($class, $column);
-            $result[$column] = $decorator->displayTitle();
-        }
-
-        return $result;
-    }
-
-    public function getClassesDropDownOptions(): array
-    {
-        return array_filter(ArrayHelper::getColumn($this->getConfigurations(), static function (array $config): ?string {
-            if (isset($config['columns']) && !empty($config['columns'])) {
-                return $config['label'];
-            }
-
-            return null;
-        }));
-    }
-
-    public function getAllPossibleColumns(): array
-    {
-        $columns = [];
-        foreach ($this->getConfigurations() as $configuration) {
-            $columns = array_merge($configuration['columns'], $columns);
-        }
-
-        return array_unique($columns);
-    }
-
-    public function getAllPossibleColumnsWithLabels(): array
-    {
-        $result = [];
-        foreach ($this->getConfigurations() as $class => $configuration) {
-            $columns = $configuration['columns'];
-            foreach ($columns as $column) {
-                $decorator = $this->getDecorator($class, $column);
-                $result[$column] = $decorator->displayTitle();
-            }
-        }
-
-        return $result;
-    }
-
-    private function getDecorator(string $class, string $type): ResourceDecoratorInterface
-    {
-        $config = $this->getConfigurationByClass($class);
-        $config['resourceModel']->type = $type;
-        /** @var ResourceDecoratorInterface $decorator */
-        $decorator = $config['resourceModel']->decorator();
-
-        return $decorator;
-    }
-
-    public function buildResourceModel(ActiveRecordInterface $resource)
-    {
-        $config = $this->getConfigurationByClass($resource->class);
-        $config['resourceModel']->setAttributes([
-            'type' => $resource->type,
-            'unit' => $resource->unit,
-            'quantity' => $resource->getAmount(),
-        ]);
-
-        return $config['resourceModel'];
-    }
-
-    public function fillTheOriginalModel(Consumption $consumption)
-    {
-        $configuration = $this->getConfigurationByClass($consumption->class);
-        $configuration['model']->setAttributes($consumption->mainObject, false);
-
-        return $configuration['model'];
-    }
-
-    public function getFirstAvailableClass(): string
-    {
-        $configurations = $this->getConfigurations();
-
-        return array_key_first($configurations);
     }
 
     /**
@@ -158,7 +45,21 @@ final class ConsumptionConfigurator
         return $this->getConfigurations()[$class] ?? $fallback;
     }
 
+    private function createObject(string $className, array $params = []): object
+    {
+        return Yii::createObject(array_merge(['class' => $className], $params));
+    }
+
     public function getConfigurations(): array
+    {
+        if ($this->configurations === null) {
+            $this->configurations = $this->buildConfigurations();
+        }
+
+        return $this->configurations;
+    }
+
+    private function buildConfigurations(): array
     {
         $configurations = [];
         /** @var ConsumptionConfigurationBehaviour $behavior */
@@ -190,8 +91,124 @@ final class ConsumptionConfigurator
         return $configurations;
     }
 
-    private function createObject(string $className, array $params = []): object
+    public function getGroupsWithLabels(string $class): array
     {
-        return Yii::createObject(array_merge(['class' => $className], $params));
+        $groups = [];
+        $columnsWithLabels = $this->getColumnsWithLabels($class);
+        foreach ($this->getGroups($class) as $i => $group) {
+            foreach ($group as $j => $type) {
+                $groups[$i][$type] = $columnsWithLabels[$type];
+            }
+        }
+
+        return $groups;
+    }
+
+    private function getGroups(string $class): array
+    {
+        $groups = [];
+        $columns = $this->getColumns($class);
+        foreach ($this->getConfigurationByClass($class)['groups'] as $group) {
+            $groups[] = $group;
+            foreach ($group as $item) {
+                $columns = array_diff($columns, [$item]);
+            }
+        }
+        foreach ($columns as $column) {
+            $groups[] = [$column];
+        }
+
+        return $groups;
+    }
+
+    public function getColumnsWithLabels(string $class): array
+    {
+        $result = [];
+        foreach ($this->getColumns($class) as $column) {
+            $decorator = $this->getDecorator($class, $column);
+
+            $result[$column] = $decorator->displayTitle();
+        }
+
+        return $result;
+    }
+
+    public function getClassesDropDownOptions(): array
+    {
+        return array_filter(ArrayHelper::getColumn($this->getConfigurations(), static function (array $config): ?string {
+            if (isset($config['columns']) && !empty($config['columns'])) {
+                return $config['label'];
+            }
+
+            return null;
+        }));
+    }
+
+    public function getAllPossibleColumns(): array
+    {
+        $columns = [];
+        foreach ($this->getConfigurations() as $configuration) {
+            $columns = array_merge($configuration['columns'], $columns);
+        }
+
+        return array_unique($columns);
+    }
+
+    public function getAllPossibleColumnsWithLabels(): array
+    {
+        if ($this->allPossibleColumnsWithLabels === null) {
+            $this->allPossibleColumnsWithLabels = [];
+            foreach ($this->getConfigurations() as $class => $configuration) {
+                $columns = $configuration['columns'];
+                foreach ($columns as $column) {
+                    $decorator = $this->getDecorator($class, $column);
+
+                    $this->allPossibleColumnsWithLabels[$column] = $decorator->displayTitle();
+                }
+            }
+        }
+
+        return $this->allPossibleColumnsWithLabels;
+    }
+
+    private function getDecorator(string $class, string $type): ResourceDecoratorInterface
+    {
+        $config = $this->getConfigurationByClass($class);
+
+        $config['resourceModel']->type = $type;
+
+        /** @var ResourceDecoratorInterface $decorator */
+        $decorator = $config['resourceModel']->decorator();
+
+        return $decorator;
+    }
+
+    public function buildResourceModel(ActiveRecordInterface $resource)
+    {
+        $config = $this->getConfigurationByClass($resource->class);
+
+        $config['resourceModel']->setAttributes([
+            'type' => $resource->type,
+            'unit' => $resource->unit,
+            'quantity' => $resource->getAmount(),
+        ]);
+
+        return $config['resourceModel'];
+    }
+
+    public function fillTheOriginalModel(Consumption $consumption)
+    {
+        $configuration = $this->getConfigurationByClass($consumption->class);
+
+        $configuration['model']->setAttributes($consumption->mainObject, false);
+
+        return $configuration['model'];
+    }
+
+    public function getFirstAvailableClass(): string
+    {
+        $configurations = $this->getConfigurations();
+
+        return array_key_first($configurations);
     }
 }
