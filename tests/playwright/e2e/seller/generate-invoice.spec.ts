1751fb7071579a522a41c9b00f9977c7e4715727
Vadym <114911409+VadymHrechukha@users.noreply.github.com>
2025-03-07 10:52:31 +0000

    HP-2382 removed code duplicate (#541)

--- tests/playwright/e2e/seller/generate-invoice.spec.ts
+++ tests/playwright/e2e/seller/generate-invoice.spec.ts
@@ -44,8 +44,7 @@ test("Test 'Generate invoice' button is work and the form opens @hipanel-module-
   await page.goto(action);
   const index = new Index(page);
   await Select2.fieldByName(page, `BillSearch[requisite_id]`).setValue(bill.requisite);
-  await index.advancedSearch.submitButton.click();
-  await page.waitForFunction(() => window.location.search.includes("BillSearch"));
+  await index.advancedSearch.submitButton();
 
   const rowNumber = await index.getRowNumberInColumnByValue("Description", bill.requisite);
   await index.chooseNumberRowOnTable(rowNumber);
