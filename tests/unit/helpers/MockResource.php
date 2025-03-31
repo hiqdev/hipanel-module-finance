9cc42000e80fa5e37d073f4093bf54895ed61bc5
Vadym Hrechukha <vadimgrechuha@gmail.com>
2025-04-01 01:08:31 +0300

    HP-1751 tiny

--- tests/unit/helpers/MockResource.php
+++ tests/unit/helpers/MockResource.php
@@ -2,21 +2,11 @@
 
 namespace hipanel\modules\finance\tests\unit\helpers;
 
-use hipanel\modules\finance\models\decorators\ResourceDecoratorFactory;
+use hipanel\modules\finance\models\HasDecorator;
 use hipanel\modules\finance\models\Resource;
 use hiqdev\billing\registry\ResourceDecorator\DecoratedInterface;
-use hiqdev\billing\registry\ResourceDecorator\ResourceDecoratorInterface;
 
 class MockResource extends Resource implements DecoratedInterface
 {
-    private ?ResourceDecoratorInterface $decorator = null;
-
-    public function decorator(): ResourceDecoratorInterface
-    {
-        if ($this->decorator === null) {
-            $this->decorator = ResourceDecoratorFactory::createFromResource($this);
-        }
-
-        return $this->decorator;
-    }
+    use HasDecorator;
 }
