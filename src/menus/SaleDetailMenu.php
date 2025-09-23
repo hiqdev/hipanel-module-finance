2ac9e005ad4ad6f2ce9d5ff4d456a1be914a2c26
Andrii Klochok <andreyklochok@gmail.com>
2025-09-23 17:49:53 +0300

    HP-2679: refactor SaleDetailMenu, enforce strict types and simplify logic (#565)

--- src/menus/SaleDetailMenu.php
+++ src/menus/SaleDetailMenu.php
@@ -1,31 +1,29 @@
 <?php
-/**
- * Finance module for HiPanel
- *
- * @link      https://github.com/hiqdev/hipanel-module-finance
- * @package   hipanel-module-finance
- * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2019, HiQDev (http://hiqdev.com/)
- */
+
+declare(strict_types=1);
+
 
 namespace hipanel\modules\finance\menus;
 
+use hipanel\modules\finance\models\Sale;
 use hipanel\widgets\AuditButton;
 use Yii;
 
 class SaleDetailMenu extends \hipanel\menus\AbstractDetailMenu
 {
-    public $model;
+    public Sale $model;
 
     public function items(): array
     {
         $actions = SaleActionsMenu::create(['model' => $this->model])->items();
-        $items = array_merge($actions, [
+
+        return array_merge($actions, [
             'delete' => [
                 'label' => Yii::t('hipanel', 'Delete'),
                 'icon' => 'fa-trash',
                 'url' => ['@sale/delete', 'id' => $this->model->id],
                 'encode' => false,
+                'visible' => Yii::$app->user->can('sale.delete'),
                 'linkOptions' => [
                     'data' => [
                         'confirm' => Yii::t('hipanel', 'Are you sure you want to delete this item?'),
@@ -37,9 +35,7 @@ class SaleDetailMenu extends \hipanel\menus\AbstractDetailMenu
             [
                 'label' => AuditButton::widget(['model' => $this->model, 'rightIcon' => true]),
                 'encode' => false,
-            ]
+            ],
         ]);
-
-        return $items;
     }
 }
