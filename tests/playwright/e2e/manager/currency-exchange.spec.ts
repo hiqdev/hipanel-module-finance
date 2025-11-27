8381f77763859eeb287f11bfb044fc5942157ba9
Andrii Klochok <andreyklochok@gmail.com>
2025-11-27 15:57:18 +0200

    HP-2787: refactor currency exchange e2e test to use AdvancedSearch an… (#573)
    * HP-2787: refactor currency exchange e2e test to use AdvancedSearch and simplify page interactions
    
    * HP-2787: fix typo in currency exchange e2e test description

--- tests/playwright/e2e/manager/currency-exchange.spec.ts
+++ tests/playwright/e2e/manager/currency-exchange.spec.ts
@@ -2,32 +2,35 @@ import { test } from "@hipanel-core/fixtures";
 import { expect } from "@playwright/test";
 import Select2 from "@hipanel-core/input/Select2";
 import Alert from "@hipanel-core/ui/Alert";
+import AdvancedSearch from "@hipanel-core/helper/AdvancedSearch";
 
-test("Test the currency exchange operation are works and creates a bill @hipanel-module-finance @manager", async ({ managerPage }) => {
-  await managerPage.goto("/finance/bill/index");
-  await expect(managerPage).toHaveTitle("Bills");
+test("the currency exchange operation works and creates a bill @hipanel-module-finance @manager", async ({ page }) => {
+  const advancedSearch = new AdvancedSearch(page);
 
-  await managerPage.locator("a:has-text(\"Currency exchange\")").click();
-  await expect(managerPage).toHaveTitle("Create currency exchange");
+  await page.goto("/finance/bill/index");
+  await expect(page).toHaveTitle("Bills");
+
+  await page.locator("a:has-text(\"Currency exchange\")").click();
+  await expect(page).toHaveTitle("Create currency exchange");
 
   await Promise.all([
-    managerPage.waitForResponse(response => response.status() === 200 && response.url().includes("get-exchange-rates")),
-    Select2.field(managerPage, "#currencyexchangeform-client_id").setValue("hipanel_test_user"),
+    page.waitForResponse(response => response.status() === 200 && response.url().includes("get-exchange-rates")),
+    Select2.field(page, "#currencyexchangeform-client_id").setValue("hipanel_test_user"),
   ]);
 
-  await Select2.field(managerPage, "#currencyexchangeform-from").setValue("USD");
-  await Select2.field(managerPage, "#currencyexchangeform-to").setValue("UAH");
-  await managerPage.locator("input[name=\"CurrencyExchangeForm\\[sum\\]\"]").fill("200");
+  await Select2.field(page, "#currencyexchangeform-from").setValue("USD");
+  await Select2.field(page, "#currencyexchangeform-to").setValue("UAH");
+  await page.locator("input[name=\"CurrencyExchangeForm\\[sum\\]\"]").fill("200");
 
-  await managerPage.locator("button:has-text(\"Create\")").click();
+  await page.locator("button:has-text(\"Create\")").click();
 
-  await Alert.on(managerPage).hasText("Currency was exchanged successfully");
+  await Alert.on(page).hasText("Currency was exchanged successfully");
 
-  await Select2.field(managerPage, "#billsearch-client_id").setValue("hipanel_test_user");
+  await advancedSearch.setFilter("client_id", "hipanel_test_user");
+  await advancedSearch.setFilter("descr", "Exchanging 200.00 USD");
+  await advancedSearch.submitButton();
 
-  await managerPage.goto(managerPage.url() + "&sort=-time");
+  await page.locator("div[role=grid] a:has-text(\"-$200.00\")").first().click();
 
-  await managerPage.locator("div[role=grid] a:has-text(\"-$200.00\")").first().click();
-
-  await expect(managerPage).toHaveTitle(/^hipanel_test_user: -200.00 usd Exchanging 200.00 USD.*/);
+  await expect(page).toHaveTitle(/^hipanel_test_user: -200.00 usd Exchanging 200.00 USD.*/);
 });
