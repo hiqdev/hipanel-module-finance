5798bf97a943c6aa16a2c5522d1ad38975490c05
Andrii Klochok <andreyklochok@gmail.com>
2025-11-28 13:36:02 +0200

    HP-2804: refactor `PriceHelper` and `progressive-price.spec.ts` for improved readability and consistency
    - Simplified constructors by utilizing `readonly` properties for `PriceHelper`.
    - Enhanced string handling in `progressive-price.spec.ts` by replacing manual ID generation with `faker` library.
    - Improved element selection and assertions in both helper and test for better clarity and maintainability.
    - Removed redundant code and optimized progressive price deletion logic.

--- tests/playwright/e2e/manager/progressive-price.spec.ts
+++ tests/playwright/e2e/manager/progressive-price.spec.ts
@@ -2,11 +2,12 @@ import { test } from "@hipanel-core/fixtures";
 import { expect } from "@playwright/test";
 import PriceHelper from "@hipanel-module-finance/Helper/PriceHelper";
 import ViewTableHelper from "@hipanel-module-finance/Helper/ViewTableHelper";
+import { faker } from "@faker-js/faker";
 
-test("Test the Progressive Price feature works @hipanel-module-finance @manager", {
+test("the Progressive Price feature works @hipanel-module-finance @manager", {
   tag: "@dedicated-server",
 }, async ({ page }) => {
-  const planName = "TEST-PROGRESSIVE-PRICE-TEMPLATE" + Math.random().toString(36).substring(7);
+  const planName = "TEST-PROGRESSIVE-PRICE-TEMPLATE-" + faker.string.nanoid(10).toUpperCase();
   const priceHelper = new PriceHelper(page);
   const viewTable = new ViewTableHelper(page);
 
@@ -20,6 +21,7 @@ test("Test the Progressive Price feature works @hipanel-module-finance @manager"
 
   await priceHelper.createProgressivePrice(planName);
 
+  await expect(page.getByText("Number of IPs")).toBeVisible();
   await expect(page.getByRole("cell", {
     name: "First 1 Item $30.00 "
       + "Next 1 Item $0.0085 (-100%) "
@@ -27,7 +29,6 @@ test("Test the Progressive Price feature works @hipanel-module-finance @manager"
       + "Over 3 Item $0.0075 (-6%) ",
   })).toBeVisible();
 
-  await expect(page.getByText("Number of IPs")).toBeVisible();
 
   page.on("dialog", async dialog => {
     await dialog.accept();
@@ -35,10 +36,6 @@ test("Test the Progressive Price feature works @hipanel-module-finance @manager"
 
   await priceHelper.deleteProgressivePriceItems();
 
-  await page.locator("input[name=\"selection_all\"]").check();
-  await page.getByRole("button", { name: "Update" }).click();
-  await expect(page.locator("input#templateprice-0-class")).toBeHidden();
-  await page.getByRole("button", { name: "Cancel" }).click();
-
-  await page.getByTestId("delete").click();
+  await expect(page.getByText("Number of IPs")).toBeVisible();
+  await expect(page.getByText("$30.00 per Item")).toBeVisible();
 });
