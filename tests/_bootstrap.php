c41b08fdfcd3a79d038e74a9d291aaf234429eb2
Andrii Klochok <andreyklochok@gmail.com>
2026-03-04 18:05:53 +0200

    php8.4: adjust error reporting level in test bootstrap file
    - Updated `error_reporting` to exclude `E_STRICT` while retaining other excluded error types.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,6 +1,7 @@
 <?php
+
 define('APP_TYPE', 'tests');
 
 require_once __DIR__ . '/../config/bootstrap.php';
 
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_DEPRECATED);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
